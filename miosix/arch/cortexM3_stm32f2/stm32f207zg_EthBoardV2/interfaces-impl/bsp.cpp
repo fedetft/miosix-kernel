7f150177670f08cab1c7af46c192382a136209e2
Terraneo Federico <fede.tft@hotmail.it>
2012-03-28 00:21:04 +0200

    Improved peripheral initialization for EthBoard

--- miosix/arch/cortexM3_stm32f2/stm32f207zg_EthBoardV2/interfaces-impl/bsp.cpp
+++ miosix/arch/cortexM3_stm32f2/stm32f207zg_EthBoardV2/interfaces-impl/bsp.cpp
@@ -59,11 +59,27 @@ void IRQbspInit()
                     RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIODEN |
                     RCC_AHB1ENR_GPIOEEN | RCC_AHB1ENR_GPIOFEN |
                     RCC_AHB1ENR_GPIOGEN;
-    //Pins used for FSMC (SRAM):
-    //PD 0,1,4,5,7,8,9,10,11,12,14,15
-    //PE 0,1,7,8,9,10,11,12,13,14,15
-    //PF 0,1,2,3,4,5,12,13,14,15
-    //PG 0,1,2,3,4,5
+
+	//Port config (H=high, L=low, PU=pullup, PD=pulldown)
+	//  |  PORTA  |  PORTB  |  PORTC  |  PORTD  |  PORTE  |  PORTF  |  PORTG  |
+	//--+---------+---------+---------+---------+---------+---------+---------+
+	// 0| AF11    | AF11    | IN      | AF12    | AF12    | AF12    | AF12    |
+	// 1| AF11    | AF11    | AF11    | AF12    | AF12    | AF12    | AF12    |
+	// 2| AF11    | IN PD   | AF11    | AF12    | OUT L   | AF12    | AF12    |
+	// 3| AF11    | AF0     | AF11    | IN PD   | IN PD   | AF12    | AF12    |
+	// 4| OUT H   | AF0     | AF11    | AF12    | IN PD   | AF12    | AF12    |
+	// 5| AF5     | AF5     | AF11    | AF12    | IN PD   | AF12    | AF12    |
+	// 6| AF5     | IN PD   | OUT L   | IN PD   | IN PD   | OUT L   | IN PD   |
+	// 7| AF11    | IN PD   | IN PD   | AF12    | AF12    | IN PU   | IN PD   |
+	// 8| AF0     | AF11    | AF12    | AF12    | AF12    | IN PD   | IN PD   |
+	// 9| AF7     | IN PD   | AF12    | AF12    | AF12    | IN PD   | IN PD   |
+	//10| AF7     | AF11    | AF12    | AF12    | AF12    | IN PD   | IN PD   |
+	//11| AF10    | AF11    | AF12    | AF12    | AF12    | IN PD   | IN PD   |
+	//12| AF10    | AF11    | AF12    | AF12    | AF12    | AF12    | IN PD   |
+	//13| AF0     | AF11    | IN      | IN PD   | AF12    | AF12    | IN PD   |
+	//14| AF0     | AF12    | IN PD   | AF12    | AF12    | AF12    | IN PD   |
+	//15| AF0     | AF12    | IN PD   | AF12    | AF12    | AF12    | IN PD   |
+    
     GPIOA->OSPEEDR=0xaaaaaaaa; //Default to 50MHz speed for all GPIOS
     GPIOB->OSPEEDR=0xaaaaaaaa; //Except SRAM GPIOs that run @ 100MHz
     GPIOC->OSPEEDR=0xaaaaaaaa;
@@ -71,100 +87,56 @@ void IRQbspInit()
     GPIOE->OSPEEDR=0xffffeaaf;
     GPIOF->OSPEEDR=0xffaaafff;
     GPIOG->OSPEEDR=0xaaaaafff;
-
-	//Port config (H=high, L=low, PU=pullup, PD=pulldown)
-	//  |  PORTA  |  PORTB  |  PORTC  |  PORTD  |  PORTE  |  PORTF  |  PORTG  |
-	//--+---------+---------+---------+---------+---------+---------+---------+
-	// 0| AF11    | AF11    | IN      | AF12    |         |         |         |
-	// 1| AF11    | AF11    | AF11    | AF12    |         |         |         |
-	// 2| AF11    | IN PD   | AF11    | AF12    |         |         |         |
-	// 3| AF11    | AF0     | AF11    | IN PD   |         |         |         |
-	// 4| OUT H   | AF0     | AF11    | AF12    |         |         |         |
-	// 5| AF5     | AF5     | AF11    | AF12    |         |         |         |
-	// 6| AF5     | IN PD   | OUT L   | IN PD   |         |         |         |
-	// 7| AF11    | IN PD   | IN PD   | AF12    |         |         |         |
-	// 8| AF0     | AF11    | AF12    | AF12    |         |         |         |
-	// 9| AF7     | IN PD   | AF12    | AF12    |         |         |         |
-	//10| AF7     | AF11    | AF12    | AF12    |         |         |         |
-	//11| AF10    | AF11    | AF12    | AF12    |         |         |         |
-	//12| AF10    | AF11    | AF12    | AF12    |         |         |         |
-	//13| AF0     | AF11    | IN PU   | IN PD   |         |         |         |
-	//14| AF0     | AF12    | IN PD   | AF12    |         |         |         |
-	//15| AF0     | AF12    | IN PD   | AF12    |         |         |         |
-	//TODO: PC0==mii_irq, requires sw pullup?
-	//PC13==sdio_cd, requires sw pu?
-    GPIOA->PUPDR=0xaaaaaaaa;
-    GPIOB->PUPDR=0xaaaaaaaa;
-    GPIOC->PUPDR=0xaaaaaaaa;
-    GPIOD->PUPDR=0xaaaaaaaa;
-    GPIOE->PUPDR=0xaaaaaaaa;
-    GPIOF->PUPDR=0xaaaaaaaa;
-    GPIOG->PUPDR=0xaaaaaaaa;
-
-	//FIXME -- begin
-	  /* Connect PDx pins to FSMC Alternate function */
-  GPIOD->AFR[0]  = 0x00cc00cc;
-  GPIOD->AFR[1]  = 0xcc0ccccc;
-  /* Configure PDx pins in Alternate function mode */
-  GPIOD->MODER   = 0xa2aa0a0a;
-  /* Configure PDx pins speed to 100 MHz */
-  GPIOD->OSPEEDR = 0xf3ff0f0f;
-  /* Configure PDx pins Output type to push-pull */
-  GPIOD->OTYPER  = 0x00000000;
-  /* No pull-up, pull-down for PDx pins */
-  GPIOD->PUPDR   = 0x00000000;
-
-  /* Connect PEx pins to FSMC Alternate function */
-  GPIOE->AFR[0]  = 0xc00000cc;
-  GPIOE->AFR[1]  = 0xcccccccc;
-  /* Configure PEx pins in Alternate function mode */
-  GPIOE->MODER   = 0xaaaa800a;
-  /* Configure PEx pins speed to 100 MHz */
-  GPIOE->OSPEEDR = 0xffffc00f;
-  /* Configure PEx pins Output type to push-pull */
-  GPIOE->OTYPER  = 0x00000000;
-  /* No pull-up, pull-down for PEx pins */
-  GPIOE->PUPDR   = 0x00000000;
-
-  /* Connect PFx pins to FSMC Alternate function */
-  GPIOF->AFR[0]  = 0x00cccccc;
-  GPIOF->AFR[1]  = 0xcccc0000;
-  /* Configure PFx pins in Alternate function mode */
-  GPIOF->MODER   = 0xaa000aaa;
-  /* Configure PFx pins speed to 100 MHz */
-  GPIOF->OSPEEDR = 0xff000fff;
-  /* Configure PFx pins Output type to push-pull */
-  GPIOF->OTYPER  = 0x00000000;
-  /* No pull-up, pull-down for PFx pins */
-  GPIOF->PUPDR   = 0x00000000;
-
-  /* Connect PGx pins to FSMC Alternate function */
-  GPIOG->AFR[0]  = 0x00cccccc;
-  GPIOG->AFR[1]  = 0x000000c0;
-  /* Configure PGx pins in Alternate function mode */
-  GPIOG->MODER   = 0x00080aaa;
-  /* Configure PGx pins speed to 100 MHz */
-  GPIOG->OSPEEDR = 0x000c0fff;
-  /* Configure PGx pins Output type to push-pull */
-  GPIOG->OTYPER  = 0x00000000;
-  /* No pull-up, pull-down for PGx pins */
-  GPIOG->PUPDR   = 0x00000000;
-
-/*-- FSMC Configuration ------------------------------------------------------*/
-  /* Enable the FSMC interface clock */
-  RCC->AHB3ENR         = 0x00000001;
-
-  /* Configure and enable Bank1_SRAM2 */
-  FSMC_Bank1->BTCR[2]  = 0x00001015;
-  FSMC_Bank1->BTCR[3]  = 0x00010400;
-  FSMC_Bank1E->BWTR[2] = 0x0fffffff;
-	//FIXME -- end
     
-    sram::cs1::mode(Mode::OUTPUT);
-    sram::cs1::high();
+    GPIOA->MODER=0xaaaaa9aa;
+    GPIOB->MODER=0xaaa20a8a;
+    GPIOC->MODER=0x02aa1aa8;
+    GPIOD->MODER=0xa2aa8a2a;
+    GPIOE->MODER=0xaaaa801a;
+    GPIOF->MODER=0xaa001aaa;
+    GPIOG->MODER=0x00000aaa;
     
-    led::mode(Mode::OUTPUT);
-    sdCardDetect::mode(Mode::INPUT_PULL_UP);
+    GPIOA->PUPDR=0x00000000;
+    GPIOB->PUPDR=0x0008a020;
+    GPIOC->PUPDR=0xa0008000;
+    GPIOD->PUPDR=0x08002080;
+    GPIOE->PUPDR=0x00002a80;
+    GPIOF->PUPDR=0x00aa4000;
+    GPIOG->PUPDR=0xaaaaa000;
+    
+    GPIOA->ODR=0x00000010;
+    GPIOB->ODR=0x00000000;
+    GPIOC->ODR=0x00000000;
+    GPIOD->ODR=0x00000000;
+    GPIOE->ODR=0x00000000;
+    GPIOF->ODR=0x00000000;
+    GPIOG->ODR=0x00000000;
+    
+    GPIOA->AFR[0]=11 | 11<<4 | 11<<8 | 11<<12 |  0<<16 |  5<<20 |  5<<24 | 11<<28;
+    GPIOA->AFR[1]= 0 |  7<<4 |  7<<8 | 10<<12 | 10<<16 |  0<<20 |  0<<24 |  0<<28;
+    GPIOB->AFR[0]=11 | 11<<4 |  0<<8 |  0<<12 |  0<<16 |  5<<20 |  0<<24 |  0<<28;
+    GPIOB->AFR[1]=11 |  0<<4 | 11<<8 | 11<<12 | 11<<16 | 11<<20 | 12<<24 | 12<<28;
+    GPIOC->AFR[0]= 0 | 11<<4 | 11<<8 | 11<<12 | 11<<16 | 11<<20 |  0<<24 |  0<<28;
+    GPIOC->AFR[1]=12 | 12<<4 | 12<<8 | 12<<12 | 12<<16 |  0<<20 |  0<<24 |  0<<28;
+    GPIOD->AFR[0]=12 | 12<<4 | 12<<8 |  0<<12 | 12<<16 | 12<<20 |  0<<24 | 12<<28;
+    GPIOD->AFR[1]=12 | 12<<4 | 12<<8 | 12<<12 | 12<<16 |  0<<20 | 12<<24 | 12<<28;
+    GPIOE->AFR[0]=12 | 12<<4 |  0<<8 |  0<<12 |  0<<16 |  0<<20 |  0<<24 | 12<<28;
+    GPIOE->AFR[1]=12 | 12<<4 | 12<<8 | 12<<12 | 12<<16 | 12<<20 | 12<<24 | 12<<28;
+    GPIOF->AFR[0]=12 | 12<<4 | 12<<8 | 12<<12 | 12<<16 | 12<<20 |  0<<24 |  0<<28;
+    GPIOF->AFR[1]= 0 |  0<<4 |  0<<8 |  0<<12 | 12<<16 | 12<<20 | 12<<24 | 12<<28;
+    GPIOG->AFR[0]=12 | 12<<4 | 12<<8 | 12<<12 | 12<<16 | 12<<20 |  0<<24 |  0<<28;
+    GPIOG->AFR[1]= 0 |  7<<4 |  7<<8 | 10<<12 | 10<<16 |  0<<20 |  0<<24 |  0<<28;
+
+    //Configure FSMC
+    RCC->AHB3ENR=RCC_AHB3ENR_FSMCEN;
+    volatile uint32_t& BCR1=FSMC_Bank1->BTCR[0];
+    volatile uint32_t& BTR1=FSMC_Bank1->BTCR[1];
+    BCR1=0x00001011; //16bit width, write enabled, SRAM mode
+    BTR1=0x00000200; //DATAST=2 FIXME: optimize!
+
+    //Configure MCO to output 25MHz clock
+    RCC->CFGR |= RCC_CFGR_MCO1_1;
+    
     ledOn();
     delayMs(100);
     ledOff();
