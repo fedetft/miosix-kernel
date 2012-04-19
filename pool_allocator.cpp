45409461fab57639a9b4d22eb491a33ee9ffda29
Terraneo Federico <fede.tft@hotmail.it>
2012-04-19 16:02:22 +0200

    First implementation of pool allocator

--- pool_allocator.cpp
+++ pool_allocator.cpp
@@ -1,26 +1,80 @@
 
 #include "pool_allocator.h"
+#include <stdexcept>
+#include <cstring>
+
+using namespace std;
 
 PoolAllocator::PoolAllocator(unsigned int *poolBase, unsigned int poolSize)
     : poolBase(poolBase), poolSize(poolSize)
 {
     bitmap=new unsigned int[poolSize/blockSize/sizeof(unsigned int)];
+    memset(bitmap,0,poolSize/blockSize);
 }
     
 unsigned int *PoolAllocator::allocate(int size)
 {
-    //TODO : align this, and validate size to prevent wraparound
+    #ifndef TEST_ALLOC
+    miosix::Lock<miosix::FastMutex> l(mutex);
+    #endif //TEST_ALLOC
+    //If size is not a power of two, or too big or small
+    if((size & (size - 1)) || size>poolSize || size<blockSize)
+            throw runtime_error("");
+    
     unsigned int offset=0;
-    if(poolBase % size) offset=size-(poolBase % size);
+    if(reinterpret_cast<unsigned int>(poolBase) % size)
+        offset=size-(reinterpret_cast<unsigned int>(poolBase) % size);
+
     int startBit=offset/blockSize;
+    int sizeBit=size/blockSize;
+    cout<<startBit<<" "<<sizeBit<<endl;
+    for(int i=startBit;i<poolSize/blockSize;i+=sizeBit)
+    {
+        bool notEmpty=false;
+        for(int j=0;j<sizeBit;j++)
+        {
+            if(testBit(i+j)==0) continue;
+            notEmpty=true;
+            break;
+        }
+        if(notEmpty) continue;
+        
+        for(int j=0;j<sizeBit;j++) setBit(i+j);
+        
+        cout<<"i="<<i<<endl;
+        unsigned int *result=poolBase+i*blockSize/4;
+        allocatedBlocks[result]=size;
+        return result;
+    }
+    throw bad_alloc();
 }
 
 void PoolAllocator::deallocate(unsigned int *ptr)
 {
-    
+    #ifndef TEST_ALLOC
+    miosix::Lock<miosix::FastMutex> l(mutex);
+    #endif //TEST_ALLOC
+    map<unsigned int*, unsigned int>::iterator it= allocatedBlocks.find(ptr);
+    if(it==allocatedBlocks.end())throw bad_alloc();
+    unsigned int size =(it->second)/blockSize;
+    unsigned int firstBit=(ptr-poolBase)/blockSize;
+    for(int i=firstBit;i<firstBit+size;i++)
+    {
+        clearBit(i);
+    }
+    allocatedBlocks.erase(it);
 }
 
 PoolAllocator::~PoolAllocator()
 {
     delete[] bitmap;
 }
+
+#ifdef TEST_ALLOC
+int main()
+{
+    PoolAllocator pool(reinterpret_cast<unsigned int*>(0x20008000),96*1024);
+    pool.allocate(65536);
+    pool.printAllocatedBlocks();
+}
+#endif //TEST_ALLOC
--- pool_allocator.h
+++ pool_allocator.h
@@ -1,6 +1,12 @@
 
 #include <map>
 
+#ifndef TEST_ALLOC
+#include <miosix.h>
+#else //TEST_ALLOC
+#include <iostream>
+#endif //TEST_ALLOC
+
 #ifndef POOL_ALLOCATOR
 #define POOL_ALLOCATOR
 
@@ -13,18 +19,50 @@ public:
     
     void deallocate(unsigned int *ptr);
     
+    #ifdef TEST_ALLOC
+    void printAllocatedBlocks()
+    {
+        using namespace std;
+        map<unsigned int*, unsigned int>::iterator it;
+        for(it=allocatedBlocks.begin();it!=allocatedBlocks.end();it++)
+            cout <<"block of size " << it->second
+                 << " allocated @ " << it->first<<endl;
+    }
+    #endif //TEST_ALLOC
+    
     ~PoolAllocator();
     
-    static const int blockSize=1024;
+    static const int blockBits=10; //size of the minimum allocatable block
+                                   //specified in bits
+    static const int blockSize=1<<blockBits;
     
 private:
     PoolAllocator(const PoolAllocator&);
     PoolAllocator& operator= (const PoolAllocator&);
     
+    bool testBit(int bit)
+    {
+        return (bitmap[bit/sizeof(unsigned int)] &
+                1<<(bit % sizeof(unsigned int))) ? true : false;
+    }
+    
+    void setBit(int bit)
+    {
+        bitmap[bit/sizeof(unsigned int)] |= 1<<(bit % sizeof(unsigned int));
+    }
+    
+    void clearBit(int bit)
+    {
+        bitmap[bit/sizeof(unsigned int)] &= ~(1<<(bit % sizeof(unsigned int)));
+    }
+    
     unsigned int *bitmap;
     unsigned int *poolBase;
     unsigned int poolSize;
     std::map<unsigned int*,unsigned int> allocatedBlocks;
+    #ifndef TEST_ALLOC
+    miosix::FastMutex mutex;
+    #endif //TEST_ALLOC
 };
 
 #endif //POOL_ALLOCATOR
