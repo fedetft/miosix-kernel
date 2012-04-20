4a56289901e8f8629cbc42c6998ff1557ad87585
Terraneo Federico <fede.tft@hotmail.it>
2012-04-20 13:04:49 +0200

    Completed allocator implementation

--- pool_allocator.cpp
+++ pool_allocator.cpp
@@ -8,8 +8,9 @@ using namespace std;
 PoolAllocator::PoolAllocator(unsigned int *poolBase, unsigned int poolSize)
     : poolBase(poolBase), poolSize(poolSize)
 {
-    bitmap=new unsigned int[poolSize/blockSize/sizeof(unsigned int)];
-    memset(bitmap,0,poolSize/blockSize);
+    int numBytes=poolSize/blockSize/8;
+    bitmap=new unsigned int[numBytes/sizeof(unsigned int)];
+    memset(bitmap,0,numBytes);
 }
     
 unsigned int *PoolAllocator::allocate(int size)
@@ -24,24 +25,21 @@ unsigned int *PoolAllocator::allocate(int size)
     unsigned int offset=0;
     if(reinterpret_cast<unsigned int>(poolBase) % size)
         offset=size-(reinterpret_cast<unsigned int>(poolBase) % size);
-
     int startBit=offset/blockSize;
     int sizeBit=size/blockSize;
-    cout<<startBit<<" "<<sizeBit<<endl;
-    for(int i=startBit;i<poolSize/blockSize;i+=sizeBit)
+
+    for(int i=startBit;i<=poolSize/blockSize;i+=sizeBit)
     {
         bool notEmpty=false;
         for(int j=0;j<sizeBit;j++)
         {
-            if(testBit(i+j)==0) continue;
+            if(testBit(i+j)==0)continue;
             notEmpty=true;
             break;
         }
         if(notEmpty) continue;
         
         for(int j=0;j<sizeBit;j++) setBit(i+j);
-        
-        cout<<"i="<<i<<endl;
         unsigned int *result=poolBase+i*blockSize/4;
         allocatedBlocks[result]=size;
         return result;
@@ -57,11 +55,9 @@ void PoolAllocator::deallocate(unsigned int *ptr)
     map<unsigned int*, unsigned int>::iterator it= allocatedBlocks.find(ptr);
     if(it==allocatedBlocks.end())throw bad_alloc();
     unsigned int size =(it->second)/blockSize;
-    unsigned int firstBit=(ptr-poolBase)/blockSize;
-    for(int i=firstBit;i<firstBit+size;i++)
-    {
-        clearBit(i);
-    }
+    unsigned int firstBit=(reinterpret_cast<unsigned int>(ptr)-
+                           reinterpret_cast<unsigned int>(poolBase))/blockSize;
+    for(int i=firstBit;i<firstBit+size;i++) clearBit(i);
     allocatedBlocks.erase(it);
 }
 
@@ -74,7 +70,39 @@ PoolAllocator::~PoolAllocator()
 int main()
 {
     PoolAllocator pool(reinterpret_cast<unsigned int*>(0x20008000),96*1024);
-    pool.allocate(65536);
-    pool.printAllocatedBlocks();
+    while(1)
+    {
+        cout<<"a<size(exponent)>|d<addr>"<<endl;
+        unsigned int param;
+        char op;
+        string line;
+        getline(cin,line);
+        stringstream ss(line);
+        ss>>op;
+        switch(op)
+        {
+            case 'a':
+                ss>>dec>>param;
+                try {
+                    pool.allocate(1<<param);
+                } catch(exception& e) {
+                    cout<<typeid(e).name();
+                }
+                pool.printAllocatedBlocks();
+                break;
+            case 'd':
+                ss>>hex>>param;
+                try {
+                    pool.deallocate(reinterpret_cast<unsigned int*>(param));
+                } catch(exception& e) {
+                    cout<<typeid(e).name();
+                }
+                pool.printAllocatedBlocks();
+                break;
+            default:
+                cout<<"Incorrect option"<<endl;
+                break;
+        }          
+    }
 }
 #endif //TEST_ALLOC
--- pool_allocator.h
+++ pool_allocator.h
@@ -5,6 +5,8 @@
 #include <miosix.h>
 #else //TEST_ALLOC
 #include <iostream>
+#include <typeinfo>
+#include <sstream>
 #endif //TEST_ALLOC
 
 #ifndef POOL_ALLOCATOR
@@ -24,9 +26,31 @@ public:
     {
         using namespace std;
         map<unsigned int*, unsigned int>::iterator it;
+        cout<<endl;
         for(it=allocatedBlocks.begin();it!=allocatedBlocks.end();it++)
             cout <<"block of size " << it->second
                  << " allocated @ " << it->first<<endl;
+        
+        cout<<endl<<endl<<"Bitmap:"<<endl;
+        const int SHIFT = 8 * sizeof( unsigned int);
+        const unsigned int MASK = 1 << (SHIFT-1);
+        int bitarray[32];
+        for(int i=0; i<(poolSize/blockSize)/(sizeof(unsigned int)*8);i++)
+        {   
+            int value=bitmap[i];
+            for ( int j = 0; j < SHIFT; j++ ) 
+            {
+                bitarray[31-j]= ( value & MASK ? 1 : 0 );
+                value <<= 1;
+            }
+            for(int j=0;j<32;j++)
+                cout<<bitarray[j];
+
+            cout << endl;
+        }
+        
+        
+        
     }
     #endif //TEST_ALLOC
     
@@ -42,18 +66,20 @@ private:
     
     bool testBit(int bit)
     {
-        return (bitmap[bit/sizeof(unsigned int)] &
-                1<<(bit % sizeof(unsigned int))) ? true : false;
+        return (bitmap[bit/(sizeof(unsigned int)*8)] &
+                1<<(bit % (sizeof(unsigned int)*8))) ? true : false;
     }
     
     void setBit(int bit)
     {
-        bitmap[bit/sizeof(unsigned int)] |= 1<<(bit % sizeof(unsigned int));
+        bitmap[(bit/(sizeof(unsigned int)*8))] |= 
+                                           1<<(bit % (sizeof(unsigned int)*8));
     }
     
     void clearBit(int bit)
     {
-        bitmap[bit/sizeof(unsigned int)] &= ~(1<<(bit % sizeof(unsigned int)));
+        bitmap[bit/(sizeof(unsigned int)*8)] &= 
+                                        ~(1<<(bit % (sizeof(unsigned int)*8)));
     }
     
     unsigned int *bitmap;
