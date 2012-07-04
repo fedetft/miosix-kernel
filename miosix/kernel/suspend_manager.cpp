bd7a982b45640e6aaa0da0872b2f88e4a92b9cbb
Terraneo Federico <fede.tft@hotmail.it>
2012-07-04 20:15:45 +0200

    Fixed copyright year

--- miosix/kernel/suspend_manager.cpp
+++ miosix/kernel/suspend_manager.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2010, 2011, 2012 by Luigi Rucco and  Terraneo Federico  *
+ *   Copyright (C) 2012 by Luigi Rucco and Terraneo Federico               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
