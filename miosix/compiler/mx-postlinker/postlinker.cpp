ea97830277b8b63fda6d80537e6c6b46f2bb788d
Terraneo Federico <fede.tft@hotmail.it>
2012-12-19 12:13:35 +0100

    Small change
    Signed-off-by: Terraneo Federico <fede.tft@hotmail.it>

--- miosix/compiler/mx-postlinker/postlinker.cpp
+++ miosix/compiler/mx-postlinker/postlinker.cpp
@@ -93,7 +93,7 @@ pair<Elf32_Dyn *,Elf32_Word> PostLinker::getDynamic()
     {
         Elf32_Phdr* phdr=getElfSegment(i);
         if(phdr->p_type!=PT_DYNAMIC) continue;
-        uint8_t* base=(uint8_t*)elf;
+        unsigned char* base=reinterpret_cast<unsigned char*>(elf);
         return make_pair(reinterpret_cast<Elf32_Dyn*>(base+phdr->p_offset),
                 phdr->p_memsz/sizeof(Elf32_Dyn));
     }
