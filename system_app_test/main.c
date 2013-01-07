46fed74056dd85d4d1f6aee15c79377ac4b731b2
Matteo <mat.p@live.com>
2013-01-07 16:05:57 +0100

    Updated app template makefiles. Done some cleanup of the source tree.

--- system_app_test/main.c
+++ system_app_test/main.c
@@ -7,13 +7,18 @@
 #include <stdio.h>
 
 int main(){
-	int x[64 * 1024];
+	//int x[64 * 1024];
+	//
+	//int i = 123;
+	//unsigned int *p = &i;
+	//
+	//p = p - 1;
+	//*p = 567;
 	
-	int i = 123;
-	unsigned int *p = &i;
-	
-	p = p - 1;
-	*p = 567;
+	volatile unsigned int *pointer = 0x63F00000;// + 0xFFFF;
+	*pointer = 456;
+	//if(*pointer == 456) write(1, "Ok\n", 3);
+	//write(1, okMsg2, mystrlen(okMsg2));
 
 	
 	return 123;
