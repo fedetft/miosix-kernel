29697d23b7b6ff94122a6bbfa8ab0dac5678f324
Matteo <mat.p@live.com>
2013-01-08 16:49:24 +0100

    Added ELF header testsuite.

---
+++ mpu_testsuite/altered_elfs/includes.h
@@ -0,0 +1,10 @@
+#ifndef _APP_MPU_TESTS_2_
+#define _APP_MPU_TESTS_2_
+#include "mpu_testsuite/altered_elfs/aelf1.h"
+#include "mpu_testsuite/altered_elfs/aelf2.h"
+#include "mpu_testsuite/altered_elfs/aelf3.h"
+#include "mpu_testsuite/altered_elfs/aelf4.h"
+#include "mpu_testsuite/altered_elfs/aelf5.h"
+#include "mpu_testsuite/altered_elfs/aelf6.h"
+#include "mpu_testsuite/altered_elfs/aelf7.h"
+#endif //_APP_MPU_TESTS_2_
---
+++ mpu_testsuite/mpu_testsuite.cpp
@@ -0,0 +1,110 @@
+#include <cstdio>
+#include <sys/wait.h>
+#include <signal.h>
+#include <stdexcept>
+#include "miosix.h"
+#include "kernel/process.h"
+
+//Include the test programs
+#include "mpu_testsuite/altered_elfs/includes.h"
+#include "mpu_testsuite/mpu_apps/includes.h"
+
+using namespace std;
+using namespace miosix;
+
+void runElfTest(const char *name, const unsigned char *filename, unsigned int file_length);
+void runMpuTest(const char *name, const unsigned char *filename, unsigned int file_length);
+
+int main()
+{
+	// Altered elfs tests
+	iprintf("\nExecuting ELF tests.\n");
+	iprintf("--------------------\n");
+	try
+	{
+		runElfTest("Test1", aelf1, aelf1_len);
+		iprintf("not passed.\n");
+	} catch (runtime_error err)
+	{
+		iprintf("passed.\n");
+	}
+	try
+	{
+		runElfTest("Test2", aelf2, aelf2_len);
+		iprintf("not passed.\n");
+	} catch (runtime_error err)
+	{
+		iprintf("passed.\n");
+	}
+	try
+	{
+		runElfTest("Test3", aelf3, aelf3_len);
+		iprintf("not passed.\n");
+	} catch (runtime_error err)
+	{
+		iprintf("passed.\n");
+	}
+	try
+	{
+		runElfTest("Test4", aelf4, aelf4_len);
+		iprintf("not passed.\n");
+	} catch (runtime_error err)
+	{
+		iprintf("passed.\n");
+	}
+	try
+	{
+		runElfTest("Test5", aelf5, aelf5_len);
+		iprintf("not passed.\n");
+	} catch (runtime_error err)
+	{
+		iprintf("passed.\n");
+	}
+	try
+	{
+		runElfTest("Test6", aelf6, aelf6_len);
+		iprintf("not passed.\n");
+	} catch (runtime_error err)
+	{
+		iprintf("passed.\n");
+	}
+	try
+	{
+		runElfTest("Test7", aelf7, aelf7_len);
+		iprintf("not passed.\n");
+	} catch (runtime_error err)
+	{
+		iprintf("passed.\n");
+	}
+	
+	// Direct mpu tests
+	iprintf("\n\nExecuting MPU tests.\n");
+	iprintf("---------------------\n");
+	runMpuTest("Test7", test1_elf, test1_elf_len);
+	runMpuTest("Test8", test2_elf, test2_elf_len);
+}
+
+void runElfTest(const char *name, const unsigned char *filename, unsigned int file_length)
+{
+	iprintf("Executing %s...", name);
+	ElfProgram prog(reinterpret_cast<const unsigned int*>(filename),file_length);
+}
+
+void runMpuTest(const char *name, const unsigned char *filename, unsigned int file_length)
+{
+	iprintf("Executing %s...\n", name);
+	ElfProgram prog(reinterpret_cast<const unsigned int*>(filename),file_length);
+	pid_t child=Process::create(prog);
+	int ec;
+	pid_t pid;
+	pid=Process::waitpid(child,&ec,0);
+	//iprintf("Process %d terminated\n",pid);
+	if(WIFEXITED(ec))
+	{
+		iprintf("not passed! (Exit status %d)\n", WEXITSTATUS(ec));
+	}
+	else if(WIFSIGNALED(ec))
+	{
+		if(WTERMSIG(ec)==SIGSEGV) iprintf("passed!\n");
+	}
+}
