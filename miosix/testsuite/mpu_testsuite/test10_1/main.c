efbe10f76fceeeaad8fb88824ddc295c5b9122f8
Luigi Rinaldi <luigi.rnl@gmail.com>
2013-01-18 17:49:29 +0100

    Fixed test 10.

--- miosix/testsuite/mpu_testsuite/test10_1/main.c
+++ miosix/testsuite/mpu_testsuite/test10_1/main.c
@@ -10,8 +10,7 @@ int main()
 {
         // This process goes to sleep for 2 seconds, while the second
         // process tries to  access the data region of this process.
-        unsigned int x = 5000000;
-        unsigned int i;
-        for(int i=0; i<x; i++)
+        unsigned int i=0;
+        for(i=0; i<5000000; i++)
         return 0;
 }
