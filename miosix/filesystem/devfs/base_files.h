dc15b3b2f3c6943541b5a3a68e18ab8356a81bbc
Terraneo Federico <fede.tft@hotmail.it>
2013-07-16 23:24:19 +0200

    Added leftover files from previous commit

---
+++ miosix/filesystem/devfs/base_files.h
@@ -0,0 +1,133 @@
+/***************************************************************************
+ *   Copyright (C) 2013 by Terraneo Federico                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   As a special exception, if other files instantiate templates or use   *
+ *   macros or inline functions from this file, or you compile this file   *
+ *   and link it with other works to produce a work based on this file,    *
+ *   this file does not by itself cause the resulting work to be covered   *
+ *   by the GNU General Public License. However the source code for this   *
+ *   file must still be made available in accordance with the GNU General  *
+ *   Public License. This exception does not invalidate any other reasons  *
+ *   why a work based on this file might be covered by the GNU General     *
+ *   Public License.                                                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#ifndef BASE_FILES_H
+#define	BASE_FILES_H
+
+#include "devfs.h"
+
+namespace miosix {
+
+/**
+ * A file where write operations do nothing at all
+ */
+class NullFile : public DeviceFile
+{
+public:
+    /**
+     * Constructor
+     * \param parent the filesystem to which this file belongs
+     */
+    NullFile() {}
+    
+    /**
+     * Write data to the file, if the file supports writing.
+     * \param data the data to write
+     * \param len the number of bytes to write
+     * \return the number of written characters, or a negative number in case
+     * of errors
+     */
+    virtual ssize_t write(const void *data, size_t len);
+    
+    /**
+     * Read data from the file, if the file supports reading.
+     * \param data buffer to store read data
+     * \param len the number of bytes to read
+     * \return the number of read characters, or a negative number in case
+     * of errors
+     */
+    virtual ssize_t read(void *data, size_t len);
+    
+    /**
+     * Move file pointer, if the file supports random-access.
+     * \param pos offset to sum to the beginning of the file, current position
+     * or end of file, depending on whence
+     * \param whence SEEK_SET, SEEK_CUR or SEEK_END
+     * \return the offset from the beginning of the file if the operation
+     * completed, or a negative number in case of errors
+     */
+    virtual off_t lseek(off_t pos, int whence);
+    
+    /**
+     * Return file information.
+     * \param pstat pointer to stat struct
+     * \return 0 on success, or a negative number on failure
+     */
+    virtual int fstat(struct stat *pstat) const;
+};
+
+/**
+ * A file where read operations return zeroed out memory
+ */
+class ZeroFile : public DeviceFile
+{
+public:
+    /**
+     * Constructor
+     * \param parent the filesystem to which this file belongs
+     */
+    ZeroFile() {}
+    
+    /**
+     * Write data to the file, if the file supports writing.
+     * \param data the data to write
+     * \param len the number of bytes to write
+     * \return the number of written characters, or a negative number in case
+     * of errors
+     */
+    virtual ssize_t write(const void *data, size_t len);
+    
+    /**
+     * Read data from the file, if the file supports reading.
+     * \param data buffer to store read data
+     * \param len the number of bytes to read
+     * \return the number of read characters, or a negative number in case
+     * of errors
+     */
+    virtual ssize_t read(void *data, size_t len);
+    
+    /**
+     * Move file pointer, if the file supports random-access.
+     * \param pos offset to sum to the beginning of the file, current position
+     * or end of file, depending on whence
+     * \param whence SEEK_SET, SEEK_CUR or SEEK_END
+     * \return the offset from the beginning of the file if the operation
+     * completed, or a negative number in case of errors
+     */
+    virtual off_t lseek(off_t pos, int whence);
+    
+    /**
+     * Return file information.
+     * \param pstat pointer to stat struct
+     * \return 0 on success, or a negative number on failure
+     */
+    virtual int fstat(struct stat *pstat) const;
+};
+
+} //namespace miosix
+
+#endif //BASE_FILES_H
