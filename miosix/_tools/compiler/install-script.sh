95d02ed8103b09485abf4cd94a495cb8bef16334
Fato Alessandro <fato.alessandro@gmail.com>
2014-03-28 17:04:45 +0100

    Added gcc patch to force addressing rodata via pc-relative accesses and data or bss through the GOT. This patch modify the gcc behaviour only when -msingle-pic-base is defined, to support miosix processes
    Signed-off-by: Terraneo Federico <fede.tft@hotmail.it>

--- miosix/_tools/compiler/install-script.sh
+++ miosix/_tools/compiler/install-script.sh
@@ -58,7 +58,8 @@ mkdir log
 # Part 2: applying patches
 #
 
-patch -p0 < patches/gcc.patch		|| quit ":: Failed patching gcc"
+patch -p0 < patches/gcc.patch		|| quit ":: Failed patching gcc 1"
+patch -p0 < patches/force-got.patch	|| quit ":: Failed patching gcc 2"
 patch -p0 < patches/newlib.patch	|| quit ":: Failed patching newlib"
 
 #
