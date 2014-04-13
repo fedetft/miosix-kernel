441f8293c06f0aebdb6ac88e250870e976c91d01
Terraneo Federico <fede.tft@miosix.org>
2014-04-13 14:28:21 +0000

    Officially releasing Miosix 2.0beta1

--- miosix/util/version.cpp
+++ miosix/util/version.cpp
@@ -37,7 +37,7 @@ namespace miosix {
 #define AU
 #endif
 
-const char AU ver[]="Miosix v2.0alpha1 (" _MIOSIX_BOARDNAME ", " __DATE__ " " __TIME__ CV ")";
+const char AU ver[]="Miosix v2.0beta1 (" _MIOSIX_BOARDNAME ", " __DATE__ " " __TIME__ CV ")";
 
 const char *getMiosixVersion()
 {
