cdfadaa7bc181efa4a002bd4c0fcf2a3f85b3b1d
Terraneo Federico <fede.tft@miosix.org>
2014-05-25 17:28:54 +0000

    netbeans IDE fixes

--- miosix/config/miosix_settings.h
+++ miosix/config/miosix_settings.h
@@ -31,7 +31,11 @@
 
 // Before you can compile the kernel you have to configure it by editing this
 // file. After that, comment out this line to disable the reminder error.
+// The PARSING_FROM_IDE is because Netbeans gets confused by this, it is never
+// defined when compiling the code.
+#ifndef PARSING_FROM_IDE
 #error This error is a reminder that you have not edited miosix_settings.h yet.
+#endif //PARSING_FROM_IDE
 
 /**
  * \file miosix_settings.h
