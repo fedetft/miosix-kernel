9f367a1c7bab6544c37b617cb940bb23050dfdd1
Terraneo Federico <fede.tft@miosix.org>
2015-02-24 23:43:37 +0000

    Added new board, oledboard2

--- miosix/config/miosix_settings.h
+++ miosix/config/miosix_settings.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2014                *
+ *   Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015          *
  *   by Terraneo Federico                                                  *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
