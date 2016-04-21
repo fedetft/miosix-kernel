e06c1dce2e1010e7b01c8f8101945a7426f7c57f
Terraneo Federico <fede.tft@miosix.org>
2016-04-21 11:35:16 +0200

    Cleaned up main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,11 @@
+
 #include <stdio.h>
-#include <pthread.h>
 #include "miosix.h"
-#include "kernel/logging.h"
-#include "interfaces/cstimer.h"
+
 using namespace std;
 using namespace miosix;
 
-static void t1Task(void* p){
-    while (true){
-        IRQbootlog("1\r\n");
-        Thread::sleep(500);
-    }
-}
-
-int main(){
-    //ContextSwitchTimer::instance();
-    Thread::setPriority(1);
-    printf("Context Switch Timer (APERIODIC) ....T=1ms\n");
-    Thread *p=Thread::create(t1Task,512,1,NULL);
-    while (true){
-        IRQbootlog("0\r\n");
-        Thread::sleep(250);
-    }
+int main()
+{
+    
 }
