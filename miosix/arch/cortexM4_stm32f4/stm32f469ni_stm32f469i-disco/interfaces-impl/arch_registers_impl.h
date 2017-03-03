71f7d8a6ee32e1504952fec4b80f30de08563434
Federico Amedeo Izzo <federico.izzo42@gmail.com>
2017-03-03 18:18:24 +0100

    Added CMSIS Include for stm32f469 and updated for stm32f4xx, fixed define of wrong board in arch_register_impl.h, completed sdram initialization for stm32f469 in bsp.cpp
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

--- miosix/arch/cortexM4_stm32f4/stm32f469ni_stm32f469i-disco/interfaces-impl/arch_registers_impl.h
+++ miosix/arch/cortexM4_stm32f4/stm32f469ni_stm32f469i-disco/interfaces-impl/arch_registers_impl.h
@@ -3,7 +3,7 @@
 #define	ARCH_REGISTERS_IMPL_H
 
 //Always include stm32f4xx.h before core_cm4.h, there's some nasty dependency
-#define STM32F429xx
+#define STM32F469xx
 #include "CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h"
 #include "CMSIS/Include/core_cm4.h"
 #include "CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h"
--- miosix/arch/cortexM4_stm32f4/stm32f469ni_stm32f469i-disco/interfaces-impl/bsp.cpp
+++ miosix/arch/cortexM4_stm32f4/stm32f469ni_stm32f469i-disco/interfaces-impl/bsp.cpp
@@ -23,7 +23,7 @@
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
- ***************************************************************************/ 
+ ***************************************************************************/
 
 /***********************************************************************
 * bsp.cpp Part of the Miosix Embedded OS.
@@ -66,6 +66,32 @@ static void sdramCommandWait()
 
 void configureSdram()
 {
+    /*   SDRAM pins assignment
+    PC0 -> FMC_SDNWE
+    PD0  -> FMC_D2   | PE0  -> FMC_NBL0 | PF0  -> FMC_A0 | PG0 -> FMC_A10
+    PD1  -> FMC_D3   | PE1  -> FMC_NBL1 | PF1  -> FMC_A1 | PG1 -> FMC_A11
+    PD8  -> FMC_D13  | PE7  -> FMC_D4   | PF2  -> FMC_A2 | PG4 -> FMC_BA0
+    PD9  -> FMC_D14  | PE8  -> FMC_D5   | PF3  -> FMC_A3 | PG5 -> FMC_BA1
+    PD10 -> FMC_D15  | PE9  -> FMC_D6   | PF4  -> FMC_A4 | PG8 -> FC_SDCLK
+    PD14 -> FMC_D0   | PE10 -> FMC_D7   | PF5  -> FMC_A5 | PG15 -> FMC_NCAS
+    PD15 -> FMC_D1   | PE11 -> FMC_D8   | PF11 -> FC_NRAS
+                     | PE12 -> FMC_D9   | PF12 -> FMC_A6
+                     | PE13 -> FMC_D10  | PF13 -> FMC_A7
+                     | PE14 -> FMC_D11  | PF14 -> FMC_A8
+                     | PE15 -> FMC_D12  | PF15 -> FMC_A9
+    PH2 -> FMC_SDCKE0| PI4 -> FMC_NBL2  |
+    PH3 -> FMC_SDNE0 | PI5 -> FMC_NBL3  |
+
+    // 32-bits Mode: D31-D16
+    PH8 -> FMC_D16   | PI0 -> FMC_D24
+    PH9 -> FMC_D17   | PI1 -> FMC_D25
+    PH10 -> FMC_D18  | PI2 -> FMC_D26
+    PH11 -> FMC_D19  | PI3 -> FMC_D27
+    PH12 -> FMC_D20  | PI6 -> FMC_D28
+    PH13 -> FMC_D21  | PI7 -> FMC_D29
+    PH14 -> FMC_D22  | PI9 -> FMC_D30
+    PH15 -> FMC_D23  | PI10 -> FMC_D31 */
+
     //Enable all gpios, passing clock
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN |
                     RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIODEN |
@@ -73,106 +99,103 @@ void configureSdram()
                     RCC_AHB1ENR_GPIOGEN | RCC_AHB1ENR_GPIOHEN |
                     RCC_AHB1ENR_GPIOIEN;
     RCC_SYNC();
-    
+
     //First, configure SDRAM GPIOs, memory controller uses AF12
-    GPIOA->AFR[0]=0xc0000000;
-    GPIOB->AFR[0]=0xccc00000;
-    GPIOC->AFR[0]=0x00cccc0c;
-    GPIOD->AFR[0]=0xccccc0cc;
-    GPIOD->AFR[1]=0xcccccccc;
-    GPIOE->AFR[0]=0xcccccccc;
+    GPIOC->AFR[0]=0x0000000c;
+    GPIOD->AFR[0]=0x000000cc;
+    GPIOD->AFR[1]=0xcc000ccc;
+    GPIOE->AFR[0]=0xc00000cc;
     GPIOE->AFR[1]=0xcccccccc;
     GPIOF->AFR[0]=0x00cccccc;
     GPIOF->AFR[1]=0xccccc000;
-    GPIOG->AFR[0]=0xc0cccccc;
-    GPIOG->AFR[1]=0xcccc0ccc;
-    GPIOH->AFR[0]=0xccc0cc00;
+    GPIOG->AFR[0]=0x00cc00cc;
+    GPIOG->AFR[1]=0xc000000c;
+    GPIOH->AFR[0]=0x0000cc00;
     GPIOH->AFR[1]=0xcccccccc;
     GPIOI->AFR[0]=0xcccccccc;
     GPIOI->AFR[1]=0x00000cc0;
 
-    //TODO
-    GPIOB->MODER=0x00002800;
     GPIOC->MODER=0x00000002;
     GPIOD->MODER=0xa02a000a;
     GPIOE->MODER=0xaaaa800a;
     GPIOF->MODER=0xaa800aaa;
     GPIOG->MODER=0x80020a0a;
-    
-    GPIOA->OSPEEDR=0xaaaaaaaa; //Default to 50MHz speed for all GPIOs...
-    GPIOB->OSPEEDR=0xaaaaaaaa | 0x00003c00; //...but 100MHz for the SDRAM pins
-    GPIOC->OSPEEDR=0xaaaaaaaa | 0x00000003;
+    GPIOH->MODER=0xaaaa00a0;
+    GPIOI->MODER=0x0028aaaa;
+
+    /* GPIO speed register
+    00: Low speed
+    01: Medium speed
+    10: High speed (50MHz)
+    11: Very high speed (100MHz) */
+
+    //Default to 50MHz speed for all GPIOs...
+    GPIOA->OSPEEDR=0xaaaaaaaa;
+    GPIOB->OSPEEDR=0xaaaaaaaa;
+    GPIOC->OSPEEDR=0xaaaaaaaa | 0x00000003; //...but 100MHz for the SDRAM pins
     GPIOD->OSPEEDR=0xaaaaaaaa | 0xf03f000f;
     GPIOE->OSPEEDR=0xaaaaaaaa | 0xffffc00f;
     GPIOF->OSPEEDR=0xaaaaaaaa | 0xffc00fff;
     GPIOG->OSPEEDR=0xaaaaaaaa | 0xc0030f0f;
-    GPIOH->OSPEEDR=0xaaaaaaaa;
-    
-    //Since we'we un-configured PB3/PB4 from the default at boot TDO,NTRST,
-    //finish the job and remove the default pull-up
-    GPIOB->PUPDR=0;
-    
+    GPIOH->OSPEEDR=0xaaaaaaaa | 0xffff00f0;
+    GPIOI->OSPEEDR=0xaaaaaaaa | 0x003cffff;
+
     //Second, actually start the SDRAM controller
     RCC->AHB3ENR |= RCC_AHB3ENR_FMCEN;
     RCC_SYNC();
-    
-    //SDRAM is a IS42S16400J -7 speed grade, connected to bank 2 (0xd0000000)
-    //Some bits in SDCR[1] are don't care, and the have to be set in SDCR[0],
-    //they aren't just don't care, the controller will fail if they aren't at 0
+
+    //SDRAM is a MT48LC4M32B2B5 -6A speed grade, connected to bank 1 (0xc0000000)
     FMC_Bank5_6->SDCR[0]=FMC_SDCR1_SDCLK_1// SDRAM runs @ half CPU frequency
                        | FMC_SDCR1_RBURST // Enable read burst
-                       | 0;               //  0 delay between reads after CAS
-    FMC_Bank5_6->SDCR[1]=0                //  8 bit column address
+                       | 0                //  0 delay between reads after CAS
+                       | 0                //  8 bit column address
                        | FMC_SDCR1_NR_0   // 12 bit row address
-                       | FMC_SDCR1_MWID_0 // 16 bit data bus
+                       | FMC_SDCR1_MWID_1 // 32 bit data bus
                        | FMC_SDCR1_NB     //  4 banks
-                       | FMC_SDCR1_CAS_1; //  2 cycle CAS latency (F<133MHz)
-    
+                       | FMC_SDCR1_CAS_0  //  3 cycle CAS latency
+                       | FMC_SDCR1_CAS_1;
+
     #ifdef SYSCLK_FREQ_180MHz
     //One SDRAM clock cycle is 11.1ns
-    //Some bits in SDTR[1] are don't care, and the have to be set in SDTR[0],
-    //they aren't just don't care, the controller will fail if they aren't at 0
-    FMC_Bank5_6->SDTR[0]=(6-1)<<12        // 6 cycle TRC  (66.6ns>63ns)
-                       | (2-1)<<20;       // 2 cycle TRP  (22.2ns>15ns)
-    FMC_Bank5_6->SDTR[1]=(2-1)<<0         // 2 cycle TMRD
-                       | (7-1)<<4         // 7 cycle TXSR (77.7ns>70ns)
+    FMC_Bank5_6->SDTR[0]=(6-1)<<12        // 6 cycle TRC  (66.6ns>60ns)
+                       | (2-1)<<20        // 2 cycle TRP  (22.2ns>18ns)
+                       | (2-1)<<0         // 2 cycle TMRD
+                       | (7-1)<<4         // 7 cycle TXSR (77.7ns>67ns)
                        | (4-1)<<8         // 4 cycle TRAS (44.4ns>42ns)
                        | (2-1)<<16        // 2 cycle TWR
-                       | (2-1)<<24;       // 2 cycle TRCD (22.2ns>15ns)
+                       | (2-1)<<24;       // 2 cycle TRCD (22.2ns>18ns)
     #elif defined(SYSCLK_FREQ_168MHz)
     //One SDRAM clock cycle is 11.9ns
-    //Some bits in SDTR[1] are don't care, and the have to be set in SDTR[0],
-    //they aren't just don't care, the controller will fail if they aren't at 0
-    FMC_Bank5_6->SDTR[0]=(6-1)<<12        // 6 cycle TRC  (71.4ns>63ns)
-                       | (2-1)<<20;       // 2 cycle TRP  (23.8ns>15ns)
-    FMC_Bank5_6->SDTR[1]=(2-1)<<0         // 2 cycle TMRD
-                       | (6-1)<<4         // 6 cycle TXSR (71.4ns>70ns)
+    FMC_Bank5_6->SDTR[0]=(6-1)<<12        // 6 cycle TRC  (71.4ns>60ns)
+                       | (2-1)<<20        // 2 cycle TRP  (23.8ns>18ns)
+                       | (2-1)<<0         // 2 cycle TMRD
+                       | (6-1)<<4         // 6 cycle TXSR (71.4ns>67ns)
                        | (4-1)<<8         // 4 cycle TRAS (47.6ns>42ns)
                        | (2-1)<<16        // 2 cycle TWR
-                       | (2-1)<<24;       // 2 cycle TRCD (23.8ns>15ns)
+                       | (2-1)<<24;       // 2 cycle TRCD (23.8ns>18ns)
     #else
     #error No SDRAM timings for this clock
     #endif
 
-    FMC_Bank5_6->SDCMR=  FMC_SDCMR_CTB2   // Enable bank 2
+    FMC_Bank5_6->SDCMR=  FMC_SDCMR_CTB1   // Enable bank 1
                        | 1;               // MODE=001 clock enabled
     sdramCommandWait();
-    
+
     //ST and SDRAM datasheet agree a 100us delay is required here.
     delayUs(100);
 
-    FMC_Bank5_6->SDCMR=  FMC_SDCMR_CTB2   // Enable bank 2
+    FMC_Bank5_6->SDCMR=  FMC_SDCMR_CTB1   // Enable bank 1
                        | 2;               // MODE=010 precharge all command
     sdramCommandWait();
-    
+
     FMC_Bank5_6->SDCMR=  (8-1)<<5         // NRFS=8 SDRAM datasheet says
                                           // "at least two AUTO REFRESH cycles"
-                       | FMC_SDCMR_CTB2   // Enable bank 2
+                       | FMC_SDCMR_CTB1   // Enable bank 1
                        | 3;               // MODE=011 auto refresh
     sdramCommandWait();
 
     FMC_Bank5_6->SDCMR=0x220<<9           // MRD=0x220:CAS latency=2 burst len=1
-                       | FMC_SDCMR_CTB2   // Enable bank 2
+                       | FMC_SDCMR_CTB1   // Enable bank 1
                        | 4;               // MODE=100 load mode register
     sdramCommandWait();
 
@@ -196,10 +219,11 @@ void IRQbspInit()
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN |
                     RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIODEN |
                     RCC_AHB1ENR_GPIOEEN | RCC_AHB1ENR_GPIOFEN |
-                    RCC_AHB1ENR_GPIOGEN | RCC_AHB1ENR_GPIOHEN;
+                    RCC_AHB1ENR_GPIOGEN | RCC_AHB1ENR_GPIOHEN |
+                    RCC_AHB1ENR_GPIOIEN;
     RCC_SYNC();
     #endif //__ENABLE_XRAM
-    
+
     _led::mode(Mode::OUTPUT);
     ledOn();
     delayMs(100);
@@ -259,7 +283,7 @@ void shutdown()
 void reboot()
 {
     ioctl(STDOUT_FILENO,IOCTL_SYNC,0);
-    
+
     #ifdef WITH_FILESYSTEM
     FilesystemManager::instance().umountAll();
     #endif //WITH_FILESYSTEM
