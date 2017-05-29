7bb236cbd1e922945e745311943952f4fac60787
Terraneo Federico <fede.tft@miosix.org>
2017-05-30 00:43:19 +0200

    Added improvements to STM32 I2C driver contributed by Silvano Seva

--- miosix/arch/common/drivers/stm32f2_f4_i2c.h
+++ miosix/arch/common/drivers/stm32f2_f4_i2c.h
@@ -1,9 +1,35 @@
+/***************************************************************************
+ *   Copyright (C) 2013 by Terraneo Federico and Silvano Seva              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   As a special exception, if other files instantiate templates or use   *
+ *   macros or inline functions from this file, or you compile this file   *
+ *   and link it with other works to produce a work based on this file,    *
+ *   this file does not by itself cause the resulting work to be covered   *
+ *   by the GNU General Public License. However the source code for this   *
+ *   file must still be made available in accordance with the GNU General  *
+ *   Public License. This exception does not invalidate any other reasons  *
+ *   why a work based on this file might be covered by the GNU General     *
+ *   Public License.                                                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/ 
+
+#ifndef STM32F2_I2C_H
+#define STM32F2_I2C_H
 
 #include <interfaces/arch_registers.h>
 #include "board_settings.h"
-
-#ifndef STM32F2_I2C_H
-#define	STM32F2_I2C_H
 
 namespace miosix {
 
@@ -32,10 +58,13 @@ public:
      * \param address device address (bit 0 is forced at 0)
      * \param data pointer with data to send
      * \param len length of data to send
+     * \param sendStop if set to false disables the sending of a stop condition
+     *                 after data transmission has finished
      * \return true on success, false on failure
      */
-    bool send(unsigned char address, const void *data, int len);
-    
+    bool send(unsigned char address, 
+            const void *data, int len, bool sendStop = true);
+            
     /**
      * Receive data from a device connected to the I2C bus
      * \param address device address (bit 0 is forced at 1) 
@@ -70,7 +99,7 @@ private:
      * send address phases of the i2c communication.
      * \return true if the operation was successful, false on error
      */
-    bool waitStatus1();
+    bool waitStatus1();    
 };
 
 } //namespace miosix
