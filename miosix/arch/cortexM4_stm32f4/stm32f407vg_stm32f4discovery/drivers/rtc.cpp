1a4a011d069ca913e1d7414c79b4213d8737b794
Daniele Marsella <daniele.marsella95@gmail.com>
2019-07-16 17:20:46 +0200

    Minor fixes
    Signed-off-by: Daniele Marsella <daniele.marsella95@gmail.com>

--- miosix/arch/cortexM4_stm32f4/stm32f407vg_stm32f4discovery/drivers/rtc.cpp
+++ miosix/arch/cortexM4_stm32f4/stm32f407vg_stm32f4discovery/drivers/rtc.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2017 by Terraneo Federico                               *
+ *   Copyright (C) 2017 by Marsella Daniele                                *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
