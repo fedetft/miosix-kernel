c40afb5ebf6ffa9f4b2ed9e9b83f211303ab5f12
Terraneo Federico <fede.tft@miosix.org>
2020-08-16 14:01:48 +0200

    Update version string

--- miosix/util/version.cpp
+++ miosix/util/version.cpp
@@ -64,7 +64,7 @@ namespace miosix {
 #define AU
 #endif
 
-const char AU ver[]="Miosix v2.11 (" _MIOSIX_BOARDNAME ", " __DATE__ " " __TIME__ CV ")";
+const char AU ver[]="Miosix v2.20 (" _MIOSIX_BOARDNAME ", " __DATE__ " " __TIME__ CV ")";
 
 const char *getMiosixVersion()
 {
