98118456f243499f5c31b8f1166832e876c034f5
Terraneo Federico <fede.tft@miosix.org>
2021-08-29 12:13:59 +0200

    Miosix now compiles again for all stm32f7 boards

--- miosix/arch/cortexM7_stm32f7/stm32f746zg_nucleo/core/stage_1_boot.cpp
+++ miosix/arch/cortexM7_stm32f7/stm32f746zg_nucleo/core/stage_1_boot.cpp
@@ -102,7 +102,7 @@ void /*__attribute__((weak))*/ UsageFault_Handler();
 void /*__attribute__((weak))*/ SVC_Handler();
 void /*__attribute__((weak))*/ DebugMon_Handler();
 void /*__attribute__((weak))*/ PendSV_Handler();
-void /*__attribute__((weak))*/ SysTick_Handler();
+void __attribute__((weak)) SysTick_Handler();
 
 //Interrupt handlers
 void __attribute__((weak)) WWDG_IRQHandler();
@@ -329,6 +329,7 @@ void (* const __Vectors[])() __attribute__ ((section(".isr_vector"))) =
 	SPDIF_RX_IRQHandler,
 };
 
+#pragma weak SysTick_IRQHandler = Default_Handler
 #pragma weak WWDG_IRQHandler = Default_Handler
 #pragma weak PVD_IRQHandler = Default_Handler
 #pragma weak TAMP_STAMP_IRQHandler = Default_Handler
