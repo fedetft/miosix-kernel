3849754c9adeb0a43df95779f1f31c2f07dee896
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2022-12-24 01:52:56 +0100

    gcc-9.2.0-mp3.1: Add support for macOS in the ramdisk script.
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

--- miosix/_tools/compiler/gcc-9.2.0-mp3.1/ramdisk.sh
+++ miosix/_tools/compiler/gcc-9.2.0-mp3.1/ramdisk.sh
@@ -1,14 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Useful script to speed up compilation and/or save write cycles on an SSD by
 # compiling in a ramdisk. Only viable if you have at least 16GByte of RAM.
 
-mkdir -p ramdisk
-sudo umount ramdisk 2> /dev/null # If not already mounted it's not an error
-sudo mount -t tmpfs -o size=7G,uid=`id -u`,gid=`id -g`,mode=700 tmpfs ramdisk
-sudo -k
+case $(uname -s) in
+	Linux)
+		mkdir -p ramdisk
+		sudo umount ramdisk 2> /dev/null # If not already mounted it's not an error
+		sudo mount -t tmpfs -o size=7G,uid=`id -u`,gid=`id -g`,mode=700 tmpfs ramdisk
+		sudo -k
+		;;
+	Darwin)
+		ramdisk_size=$(( 7 * 1024 * 1024 ))
+		ramdisk_name="miosix_ramdisk_$RANDOM"
+		diskutil erasevolume HFS+ "$ramdisk_name" $(hdiutil attach -nobrowse -nomount ram://$(( ramdisk_size ))) > /dev/stderr
+		ln -s "/Volumes/$ramdisk_name/" ./ramdisk
+		;;
+	*)
+		echo "error: I don't know how to make a ramdisk on platform " $(uname -s) > /dev/stderr
+		exit 1
+		;;
+esac
 
-cp -R downloaded ramdisk
+ln -s `pwd`/downloaded ramdisk/downloaded
 cp -R installers ramdisk
 cp -R mx-postlinker ramdisk
 cp -R patches ramdisk
