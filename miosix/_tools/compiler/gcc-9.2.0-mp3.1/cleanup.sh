5dc4c8c5f446679aa6f577a6fc01f1c70bf56acc
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2022-12-24 01:50:36 +0100

    gcc-9.2.0-mp3.1: Update GMP to 6.2.1.
    Fixes some bugs exposed by newer clang versions.
    
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

--- miosix/_tools/compiler/gcc-9.2.0-mp3.1/cleanup.sh
+++ miosix/_tools/compiler/gcc-9.2.0-mp3.1/cleanup.sh
@@ -5,10 +5,10 @@
 # running install-script.sh is possible without re-downloading them.
 
 rm -rf binutils-2.32 gcc-9.2.0 gdb-9.1 gdb-obj newlib-3.1.0 newlib-obj \
-	gmp-6.1.2 mpfr-4.0.2 mpc-1.1.0 make-4.2.1 expat-2.2.9 ncurses-6.1 \
+	gmp-6.2.1 mpfr-4.0.2 mpc-1.1.0 make-4.2.1 expat-2.2.9 ncurses-6.1 \
 	makeself-2.4.0 lib quickfix lpc21isp.c
 
 rm -rf objdir/ log/
 if [[ $? -ne 0 ]]; then
-    sudo rm -rf objdir/ log/
+	sudo rm -rf objdir/ log/
 fi
--- miosix/_tools/compiler/gcc-9.2.0-mp3.1/download.sh
+++ miosix/_tools/compiler/gcc-9.2.0-mp3.1/download.sh
@@ -10,6 +10,6 @@ wget https://ftpmirror.gnu.org/binutils/binutils-2.32.tar.xz
 wget https://ftpmirror.gnu.org/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz
 wget ftp://sourceware.org/pub/newlib/newlib-3.1.0.tar.gz
 wget https://ftpmirror.gnu.org/gdb/gdb-9.1.tar.xz
-wget https://ftpmirror.gnu.org/gmp/gmp-6.1.2.tar.xz
+wget https://ftpmirror.gnu.org/gmp/gmp-6.2.1.tar.xz
 wget https://ftpmirror.gnu.org/mpfr/mpfr-4.0.2.tar.xz
 wget https://ftpmirror.gnu.org/mpc/mpc-1.1.0.tar.gz
