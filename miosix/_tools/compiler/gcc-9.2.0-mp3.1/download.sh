85c179e83fa723648c79474827ffbd2492110049
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2023-01-04 17:09:40 +0100

    gcc-9.2.0-mp3.1: Use curl when wget is not available.
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

--- miosix/_tools/compiler/gcc-9.2.0-mp3.1/download.sh
+++ miosix/_tools/compiler/gcc-9.2.0-mp3.1/download.sh
@@ -6,10 +6,17 @@
 mkdir downloaded || exit
 cd downloaded
 
-wget https://ftpmirror.gnu.org/binutils/binutils-2.32.tar.xz
-wget https://ftpmirror.gnu.org/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz
-wget ftp://sourceware.org/pub/newlib/newlib-3.1.0.tar.gz
-wget https://ftpmirror.gnu.org/gdb/gdb-9.1.tar.xz
-wget https://ftpmirror.gnu.org/gmp/gmp-6.2.1.tar.xz
-wget https://ftpmirror.gnu.org/mpfr/mpfr-4.0.2.tar.xz
-wget https://ftpmirror.gnu.org/mpc/mpc-1.1.0.tar.gz
+# macOS does not ship with wget, check if it exists and otherwise use curl
+if command -v wget > /dev/null; then
+	WGET=wget
+else
+	WGET='curl -LO'
+fi
+
+$WGET https://ftpmirror.gnu.org/binutils/binutils-2.32.tar.xz
+$WGET https://ftpmirror.gnu.org/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz
+$WGET ftp://sourceware.org/pub/newlib/newlib-3.1.0.tar.gz
+$WGET https://ftpmirror.gnu.org/gdb/gdb-9.1.tar.xz
+$WGET https://ftpmirror.gnu.org/gmp/gmp-6.2.1.tar.xz
+$WGET https://ftpmirror.gnu.org/mpfr/mpfr-4.0.2.tar.xz
+$WGET https://ftpmirror.gnu.org/mpc/mpc-1.1.0.tar.gz
--- miosix/_tools/compiler/gcc-9.2.0-mp3.1/installers/additional-download.sh
+++ miosix/_tools/compiler/gcc-9.2.0-mp3.1/installers/additional-download.sh
@@ -6,16 +6,23 @@
 # Meant to be run from the main compiler directory (./installers/additional-download.sh)
 cd downloaded || exit
 
+# macOS does not ship with wget, check if it exists and otherwise use curl
+if command -v wget > /dev/null; then
+	WGET=wget
+else
+	WGET='curl -LO'
+fi
+
 # Linux
-wget https://ftpmirror.gnu.org/ncurses/ncurses-6.1.tar.gz
-wget https://github.com/megastep/makeself/releases/download/release-2.4.5/makeself-2.4.5.run
+$WGET https://ftpmirror.gnu.org/ncurses/ncurses-6.1.tar.gz
+$WGET https://github.com/megastep/makeself/releases/download/release-2.4.5/makeself-2.4.5.run
 
 # Windows
-wget https://ftpmirror.gnu.org/make/make-4.2.1.tar.gz
-wget https://github.com/fpoussin/QStlink2/releases/download/v1.2.2/qstlink2_1.2.2.exe
+$WGET https://ftpmirror.gnu.org/make/make-4.2.1.tar.gz
+$WGET https://github.com/fpoussin/QStlink2/releases/download/v1.2.2/qstlink2_1.2.2.exe
 mv qstlink2_1.2.2.exe qstlink2.exe
-wget https://jrsoftware.org/download.php/is.exe
+$WGET https://jrsoftware.org/download.php/is.exe
 mv is.exe innosetup.exe
 
 # All
-wget https://github.com/libexpat/libexpat/releases/download/R_2_2_10/expat-2.2.10.tar.xz
+$WGET https://github.com/libexpat/libexpat/releases/download/R_2_2_10/expat-2.2.10.tar.xz
