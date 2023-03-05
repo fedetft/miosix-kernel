d1c2819e851aab7a38c2cfcbab26fb6cceebbb5a
Terraneo Federico <fede.tft@miosix.org>
2023-03-05 11:24:50 +0100

    Minor changes, did not fix the issue

--- miosix/kernel/scheduler/timer_interrupt.h
+++ miosix/kernel/scheduler/timer_interrupt.h
@@ -51,6 +51,7 @@ inline bool IRQtimerInterrupt(long long currentTime)
     if(currentTime >= Scheduler::IRQgetNextPreemption() || hptw)
     {
         //End of the burst || a higher priority thread has woken up
+        //TODO: IRQinvokeScheduler() returns the burst time, but is only called here and return value is ignored
         IRQinvokeScheduler(); //If the kernel is running, preempt
         return false;
     }
