feae0c2343f3345c07215552f25a119952180825
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2023-12-26 23:55:11 +0100

    LittleFS: Fix copyright headers.

--- miosix/filesystem/littlefs/lfs_miosix.h
+++ miosix/filesystem/littlefs/lfs_miosix.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2023 by Andrea Somaini                                  *
+ *   Copyright (C) 2023 by Andrea Somaini, Simone Paternoster              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
