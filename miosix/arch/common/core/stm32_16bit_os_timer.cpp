90e46eaaa3c8b734829af8dff543e6bfc67d819d
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2024-01-11 11:59:38 +0100

    Merge all 16-bit timer implementations for STM32 chips in a single file.
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

--- miosix/arch/common/core/stm32_16bit_os_timer.cpp
+++ miosix/arch/common/core/stm32_16bit_os_timer.cpp
@@ -1,6 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2021 by Federico Terraneo                               *
- *   Copyright (C) 2023 by Daniele Cattaneo                                *
+ *   Copyright (C) 2021 by Terraneo Federico                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -33,25 +32,78 @@
 
 namespace miosix {
 
-class STM32Timer22 : public TimerAdapter<STM32Timer22, 16>
+#if defined(_ARCH_CORTEXM0PLUS_STM32L0)
+
+class STM32Timer22HW
 {
 public:
-    static inline unsigned int IRQgetTimerCounter() { return TIM22->CNT; }
-    static inline void IRQsetTimerCounter(unsigned int v) { TIM22->CNT=v; }
+    static inline TIM_TypeDef *get() { return TIM22; }
+    static inline IRQn_Type getIRQn() { return TIM22_IRQn; }
+    static inline unsigned int IRQgetClock()
+    {
+        unsigned int timerInputFreq=SystemCoreClock;
+        if(RCC->CFGR & RCC_CFGR_PPRE2_2) timerInputFreq/=1<<((RCC->CFGR>>RCC_CFGR_PPRE2_Pos) & 0x3);
+        return timerInputFreq;
+    }
+    static inline void IRQenable()
+    {
+        RCC->APB2ENR |= RCC_APB2ENR_TIM22EN;
+        RCC_SYNC();
+        DBGMCU->APB2FZ |= DBGMCU_APB2_FZ_DBG_TIM22_STOP; //Stop while debugging
+    }
+};
+#define IRQ_HANDLER_NAME TIM22_IRQHandler
+#define TIMER_HW_CLASS STM32Timer22HW
 
-    static inline unsigned int IRQgetTimerMatchReg() { return TIM22->CCR1; }
-    static inline void IRQsetTimerMatchReg(unsigned int v) { TIM22->CCR1=v; }
+#else
 
-    static inline bool IRQgetOverflowFlag() { return TIM22->SR & TIM_SR_UIF; }
-    static inline void IRQclearOverflowFlag() { TIM22->SR = ~TIM_SR_UIF; }
+class STM32Timer4HW
+{
+public:
+    static inline TIM_TypeDef *get() { return TIM4; }
+    static inline IRQn_Type getIRQn() { return TIM4_IRQn; }
+    static inline unsigned int IRQgetClock()
+    {
+        unsigned int timerInputFreq=SystemCoreClock;
+        if(RCC->CFGR & RCC_CFGR_PPRE1_2) timerInputFreq/=1<<((RCC->CFGR>>RCC_CFGR_PPRE1_Pos) & 0x3);
+        return timerInputFreq;
+    }
+    static inline void IRQenable()
+    {
+        RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+        RCC_SYNC();
+        #ifndef _ARCH_CORTEXM3_STM32L1
+        DBGMCU->CR |= DBGMCU_CR_DBG_TIM4_STOP; //Stop while debugging
+        #else
+        DBGMCU->APB1FZ |= DBGMCU_APB1_FZ_DBG_TIM4_STOP; //Stop while debugging
+        #endif
+    }
+};
+#define IRQ_HANDLER_NAME TIM4_IRQHandler
+#define TIMER_HW_CLASS STM32Timer4HW
+
+#endif
+
+template<class T>
+class STM32Timer : public TimerAdapter<STM32Timer<T>, 16>
+{
+public:
+    static inline unsigned int IRQgetTimerCounter() { return T::get()->CNT; }
+    static inline void IRQsetTimerCounter(unsigned int v) { T::get()->CNT=v; }
+
+    static inline unsigned int IRQgetTimerMatchReg() { return T::get()->CCR1; }
+    static inline void IRQsetTimerMatchReg(unsigned int v) { T::get()->CCR1=v; }
+
+    static inline bool IRQgetOverflowFlag() { return T::get()->SR & TIM_SR_UIF; }
+    static inline void IRQclearOverflowFlag() { T::get()->SR = ~TIM_SR_UIF; }
     
-    static inline bool IRQgetMatchFlag() { return TIM22->SR & TIM_SR_CC1IF; }
-    static inline void IRQclearMatchFlag() { TIM22->SR = ~TIM_SR_CC1IF; }
+    static inline bool IRQgetMatchFlag() { return T::get()->SR & TIM_SR_CC1IF; }
+    static inline void IRQclearMatchFlag() { T::get()->SR = ~TIM_SR_CC1IF; }
     
-    static inline void IRQforcePendingIrq() { NVIC_SetPendingIRQ(TIM22_IRQn); }
+    static inline void IRQforcePendingIrq() { NVIC_SetPendingIRQ(T::getIRQn()); }
 
-    static inline void IRQstopTimer() { TIM22->CR1 &= ~TIM_CR1_CEN; }
-    static inline void IRQstartTimer() { TIM22->CR1 |= TIM_CR1_CEN; }
+    static inline void IRQstopTimer() { T::get()->CR1 &= ~TIM_CR1_CEN; }
+    static inline void IRQstartTimer() { T::get()->CR1 |= TIM_CR1_CEN; }
     
     /*
      * These microcontrollers unfortunately do not have a 32bit timer.
@@ -70,36 +122,35 @@ public:
     
     static void IRQinitTimer()
     {
-        RCC->APB2ENR |= RCC_APB2ENR_TIM22EN;
-        RCC_SYNC();
-        DBGMCU->APB2FZ |= DBGMCU_APB2_FZ_DBG_TIM22_STOP; //Stop while debugging
-        // Setup TIM22 base configuration
+        T::IRQenable();
+        // Setup TIM4 base configuration
         // Mode: Up-counter
         // Interrupts: counter overflow, Compare/Capture on channel 1
-        TIM22->CR1=TIM_CR1_URS;
-        TIM22->DIER=TIM_DIER_UIE | TIM_DIER_CC1IE;
-        NVIC_SetPriority(TIM22_IRQn,3); //High priority for TIM22 (Max=0, min=15)
-        NVIC_EnableIRQ(TIM22_IRQn);
+        T::get()->CR1=TIM_CR1_URS;
+        T::get()->DIER=TIM_DIER_UIE | TIM_DIER_CC1IE;
+        NVIC_SetPriority(T::getIRQn(),3); //High priority for TIM4 (Max=0, min=15)
+        NVIC_EnableIRQ(T::getIRQn());
         // Configure channel 1 as:
         // Output channel (CC1S=0)
         // No preload(OC1PE=0), hence TIM4_CCR1 can be written at anytime
         // No effect on the output signal on match (OC1M = 0)
-        TIM22->CCMR1 = 0;
-        TIM22->CCR1 = 0;
-        // TIM22 Operation Frequency Configuration: Max Freq. and longest period
+        T::get()->CCMR1 = 0;
+        T::get()->CCR1 = 0;
+        // TIM4 Operation Frequency Configuration: Max Freq. and longest period
         
         // The global variable SystemCoreClock from ARM's CMSIS allows to know
         // the CPU frequency.
-        unsigned int timerInputFreq=SystemCoreClock;
-
-        // The timer frequency may however be a submultiple of the CPU frequency,
+        // However, the timer frequency may be a submultiple of the CPU frequency,
         // due to the bus at whch the periheral is connected being slower. The
-        // RCC->CFGR register tells us how slower the APB1 bus is running.
-        // This formula takes into account that if the APB1 clock is divided by a
+        // RCC->CFGR register tells us how slower the APB bus is running.
+        // This formula takes into account that if the APB clock is divided by a
         // factor of two or greater, the timer is clocked at twice the bus
         // interface. After this, the freq variable contains the frequency in Hz
         // at which the timer prescaler is clocked.
-        if(RCC->CFGR & RCC_CFGR_PPRE1_2) timerInputFreq/=1<<((RCC->CFGR>>8) & 0x3);
+        // The field in the RCC registers where we need to look at depends on
+        // the specific timer, so this computation is defined in the hardware
+        // abstraction class.
+        unsigned int timerInputFreq=T::IRQgetClock();
         
         //Handle the case when the prescribed timer frequency is not achievable.
         //For now, we just enter an infinite loop so if someone selects an
@@ -110,17 +161,17 @@ public:
             for(;;) ;
         }
         
-        TIM22->PSC = (timerInputFreq/timerFrequency)-1;
-        TIM22->ARR = 0xFFFF;
-        TIM22->EGR = TIM_EGR_UG; //To enforce the timer to apply PSC
+        T::get()->PSC = (timerInputFreq/timerFrequency)-1;
+        T::get()->ARR = 0xFFFF;
+        T::get()->EGR = TIM_EGR_UG; //To enforce the timer to apply PSC
     }
 };
 
-static STM32Timer22 timer;
+static STM32Timer<TIMER_HW_CLASS> timer;
 DEFAULT_OS_TIMER_INTERFACE_IMPLMENTATION(timer);
 } //namespace miosix
 
-void __attribute__((naked)) TIM22_IRQHandler()
+void __attribute__((naked)) IRQ_HANDLER_NAME()
 {
     saveContext();
     asm volatile ("bl _Z11osTimerImplv");
