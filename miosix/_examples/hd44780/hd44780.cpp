7406329e11a0bb71e721a1d42d9e7ff58a4dc0ba
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2024-04-20 17:17:03 +0200

    hd44780 example: Also demonstrate custom characters.
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

--- miosix/_examples/hd44780/hd44780.cpp
+++ miosix/_examples/hd44780/hd44780.cpp
@@ -15,6 +15,27 @@ typedef Gpio<GPIOB_BASE,15> d7;
 typedef Gpio<GPIOC_BASE,1> rs;
 typedef Gpio<GPIOC_BASE,2> e;
 
+// Custom characters:
+//   0      1      2      3      4      5      6      7
+// ..X..  .....  .....  .....  .....  .....  .....  .....
+// ..X..  ....X  .....  .....  .....  .....  .....  X....
+// ..X..  ...X.  .....  .....  .....  .....  .....  .X...
+// ..X..  ..X..  ..XXX  ..X..  ..X..  ..X..  XXX..  ..X..
+// .....  .....  .....  ...X.  ..X..  .X...  .....  .....
+// .....  .....  .....  ....X  ..X..  X....  .....  .....
+// .....  .....  .....  .....  ..X..  .....  .....  .....
+// .....  .....  .....  .....  .....  .....  .....  .....
+static const unsigned char clockFont[8][8] = {
+    { 0x04,0x04,0x04,0x04,0x00,0x00,0x00,0x00 },
+    { 0x00,0x01,0x02,0x04,0x00,0x00,0x00,0x00 },
+    { 0x00,0x00,0x00,0x07,0x00,0x00,0x00,0x00 }, 
+    { 0x00,0x00,0x00,0x04,0x02,0x01,0x00,0x00 }, 
+    { 0x00,0x00,0x00,0x04,0x04,0x04,0x04,0x00 }, 
+    { 0x00,0x00,0x00,0x04,0x08,0x10,0x00,0x00 }, 
+    { 0x00,0x00,0x00,0x1C,0x00,0x00,0x00,0x00 }, 
+    { 0x00,0x10,0x08,0x04,0x00,0x00,0x00,0x00 }
+};
+
 int main()
 {
     //First 6 parameters are GPIOs, then there are the number of lines
@@ -22,12 +43,13 @@ int main()
     Lcd44780 display(rs::getPin(),e::getPin(),d4::getPin(),
                      d5::getPin(),d6::getPin(),d7::getPin(),2,16);
     display.clear();
+    for(int i=0; i<8; i++) display.setCgram(i, clockFont[i]);
     display.go(0,0);
     display.printf("Miosix + HD44780");
     for(int i=0;;i++)
     {
+        display.go(2,1);
+        display.printf("[ %c i=%04d ]",i%8,i);
         Thread::sleep(1000);
-        display.go(0,1);
-        display.printf("i=%04d",i);
     }
 }
