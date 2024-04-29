de6ec61e7464e013b6ebd5dd2f24413dc3fb807c
Terraneo Federico <fede.tft@miosix.org>
2024-04-29 11:57:23 +0200

    Replace macros wuth constexpr functions, fix CMD_SHIFT

--- miosix/util/lcd44780.cpp
+++ miosix/util/lcd44780.cpp
@@ -5,38 +5,61 @@
 #include "interfaces/delays.h"
 #include "lcd44780.h"
 
+namespace {
+
 /// Fills data RAM with spaces, sets cursor address to 0, resets scrolling,
 /// enables increment mode.
-#define CMD_CLEAR                   (0x01)
+constexpr unsigned char cmdClear=0x01;
 /// Sets cursor address to 0 and resets scrolling
-#define CMD_HOME                    (0x02)
+constexpr unsigned char cmdHome=0x02;
 /// Configures display update behaviour at every data byte sent.
-/// @param dir   0: decrement data RAM address at every byte sent
+/// \param dir   0: decrement data RAM address at every byte sent
 ///              1: increment data RAM address at every byte sent 
-/// @param shift 0: sending data does not affect scrolling
+/// \param shift 0: sending data does not affect scrolling
 ///              1: sending data scrolls the display left/right depending on
 ///                 the data RAM address increment/decrement mode
-#define CMD_ENTRY_MODE(dir,shift)   (0x04|((dir)<<1)|(shift))
+constexpr unsigned char cmdEntryMode(unsigned char dir, unsigned char shift)
+{
+    return 0x04 | (dir<<1) | shift;
+}
 /// Enables and disables display layers
-/// @param disp  0: turns off the display, 1: turns on the display
-/// @param cur   Cursor mode (see Cursor enum)
-#define CMD_ENABLE(disp,cur)        (0x08|((disp)<<2)|(cur))
+/// \param disp  0: turns off the display, 1: turns on the display
+/// \param cur   Cursor mode (see Cursor enum)
+constexpr unsigned char cmdEnable(unsigned char disp, unsigned char cur)
+{
+    return 0x08 | (disp<<2) | cur;
+}
 /// Move the cursor or scroll the display by 1 character forwards or backwards
-/// @param shift 0: moves the cursor, 1: scrolls the display
-/// @param rl    0: left (increment), 1: right (decrement)
-#define CMD_SHIFT(shift,dir)        (0x10|((sc)<<3)|((rl)<<2))
+/// \param shift 0: moves the cursor, 1: scrolls the display
+/// \param rl    0: left (increment), 1: right (decrement)
+constexpr unsigned char cmdShift(unsigned char shift, unsigned char dir)
+{
+    return 0x10 | (shift<<3) | (dir<<2);
+}
 /// Configures the controller for the display
-/// @param dl    Data length
+/// \param dl    Data length
 ///              0: 4 bit, 1: 8 bit
-/// @param lines Number of display lines
+/// \param lines Number of display lines
 ///              0: 1 line, 1: 2 lines
-/// @param font  Size of the font (only for 1 line displays, otherwise ignored)
+/// \param font  Size of the font (only for 1 line displays, otherwise ignored)
 ///              0: 5x8m 1: 5x10
-#define CMD_FUNC_SET(dl,lines,font) (0x20|((dl)<<4)|((lines)<<3)|((font)<<2))
+constexpr unsigned char cmdFuncSet(unsigned char dl, unsigned char lines,
+                                     unsigned char font)
+{
+    return 0x20 | (dl<<4) | (lines<<3) | (font<<2);
+}
 /// Sets the current write address in the font data RAM
-#define CMD_CG_ADDR_SET(addr)       (0x40|(addr))
+constexpr unsigned char cmdCgAddrSet(unsigned char addr)
+{
+    return 0x40 | addr;
+}
 /// Sets the current write address in the display data RAM
-#define CMD_DD_ADDR_SET(addr)       (0x80|(addr))
+constexpr unsigned char cmdDdAddrSet(unsigned char addr)
+{
+    return 0x80 | addr;
+}
+
+} //anonymous namespace
 
 namespace miosix {
 
@@ -64,7 +87,7 @@ void Lcd44780::go(int x, int y)
     // 4x20 is implemented as 2x40.
     if(y>1) x += col;
 
-    comd(CMD_DD_ADDR_SET(((y & 1) ? 0x40 : 0) | x)); //Move cursor
+    comd(cmdDdAddrSet(((y & 1) ? 0x40 : 0) | x)); //Move cursor
 }
 
 int Lcd44780::iprintf(const char* fmt, ...)
@@ -91,31 +114,31 @@ int Lcd44780::printf(const char* fmt, ...)
 
 void Lcd44780::clear()
 {
-    comd(CMD_CLEAR);
+    comd(cmdClear);
     Thread::sleep(2); //Some displays require this delay
 }
 
 void Lcd44780::off()
 {
-    comd(CMD_ENABLE(0,0));
+    comd(cmdEnable(0,0));
 }
 
 void Lcd44780::on()
 {
-    comd(CMD_ENABLE(1,cursorState));
+    comd(cmdEnable(1,cursorState));
 }
 
 void Lcd44780::cursor(Cursor cursor)
 {
     cursorState=cursor & 0x3;
-    comd(CMD_ENABLE(1,cursorState));
+    comd(cmdEnable(1,cursorState));
 }
 
 void Lcd44780::setCgram(int charCode, const unsigned char font[8])
 {
-    comd(CMD_CG_ADDR_SET((charCode & 0x7)<<3));
+    comd(cmdCgAddrSet((charCode & 0x7)<<3));
     for(int i=0;i<8;i++) data(font[i]);
-    comd(CMD_DD_ADDR_SET(0));
+    comd(cmdDdAddrSet(0));
 }
 
 void Lcd44780::init()
@@ -141,20 +164,20 @@ void Lcd44780::init()
     //      The upper nybble of the first command is undefined. However the
     //      second command switches the controller to 8 bit mode.
     rs.low();
-    half(CMD_FUNC_SET(1,0,0));
+    half(cmdFuncSet(1,0,0));
     delayUs(50);
-    half(CMD_FUNC_SET(1,0,0));
+    half(cmdFuncSet(1,0,0));
     delayUs(50);
-    half(CMD_FUNC_SET(1,0,0));
+    half(cmdFuncSet(1,0,0));
     delayUs(50);
     // Now we can safely switch the controller to 4 bit mode
-    half(CMD_FUNC_SET(0,0,0));
+    half(cmdFuncSet(0,0,0));
     rs.high();
     delayUs(50);
-    if(row==1) comd(CMD_FUNC_SET(0,0,0)); else comd(CMD_FUNC_SET(0,1,0));
+    if(row==1) comd(cmdFuncSet(0,0,0)); else comd(cmdFuncSet(0,1,0));
     Thread::sleep(5);  //Initialization delay
-    comd(CMD_ENABLE(1,cursorState)); //Display ON, cursor OFF, blink OFF
-    comd(CMD_ENTRY_MODE(1,0));  //Auto increment ON, shift OFF
+    comd(cmdEnable(1,cursorState)); //Display ON, cursor OFF, blink OFF
+    comd(cmdEntryMode(1,0));  //Auto increment ON, shift OFF
 }
 
 void Lcd44780::half(unsigned char byte)
