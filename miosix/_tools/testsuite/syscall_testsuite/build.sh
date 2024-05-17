759af4aff7ea9fe49456016727793c45c4999e67
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2024-05-17 15:34:01 +0200

    testsuite: Remove old process tests
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

--- miosix/_tools/testsuite/syscall_testsuite/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in *;
-do
-	if test -d $i; then
-		cd $i && make clean && make "NAME=$i" && mv prog3.h "../$i.h" && cd ..
-	fi;
-done;
--- miosix/_tools/testsuite/syscall_testsuite/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in *;
-do
-	if test -d $i; then
-		rm "$i.h"
-		cd $i && make clean && cd ..
-	fi;
-done;
