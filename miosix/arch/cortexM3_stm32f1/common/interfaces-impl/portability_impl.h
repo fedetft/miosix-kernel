ed4f3370e5c22960dd33c90f897ea9e7e2c3a449
Terraneo Federico <fede.tft@miosix.org>
2024-06-14 11:08:01 +0200

    Add process support to stm32f1

--- miosix/arch/cortexM3_stm32f1/common/interfaces-impl/portability_impl.h
+++ miosix/arch/cortexM3_stm32f1/common/interfaces-impl/portability_impl.h
@@ -30,7 +30,9 @@
 #define PORTABILITY_IMPL_H
 
 #include "interfaces/arch_registers.h"
+#include "interfaces/portability.h"
 #include "config/miosix_settings.h"
+#include <cassert>
 
 /**
  * \addtogroup Drivers
@@ -43,7 +45,7 @@
  * context switch. It requires C linkage to be used inside asm statement.
  * Registers are saved in the following order:
  * *ctxsave+32 --> r11
- * *ctxsave+28 --> r10
+ * *ctxsave+28 --> r10        
  * *ctxsave+24 --> r9
  * *ctxsave+20 --> r8
  * *ctxsave+16 --> r7
@@ -140,6 +142,54 @@ inline bool checkAreInterruptsEnabled()
     if(i!=0) return false;
     return true;
 }
+
+#ifdef WITH_PROCESSES
+
+namespace {
+/*
+ * ARM syscall parameter mapping
+ * Syscall id is r3, saved at registers[3]
+ *
+ * Parameter 1 is r0, saved at registers[0]
+ * Parameter 2 is r1, saved at registers[1]
+ * Parameter 3 is r2, saved at registers[2]
+ * Parameter 4 is r12, saved at registers[4]
+ */
+constexpr unsigned int armSyscallMapping[]={0,1,2,4};
+}
+
+//
+// class SyscallParameters
+//
+
+inline SyscallParameters::SyscallParameters(unsigned int *context) :
+        registers(reinterpret_cast<unsigned int*>(context[0])) {}
+
+inline int SyscallParameters::getSyscallId() const
+{
+    return registers[3];
+}
+
+inline unsigned int SyscallParameters::getParameter(unsigned int index) const
+{
+    assert(index<4);
+    return registers[armSyscallMapping[index]];
+}
+
+inline void SyscallParameters::setParameter(unsigned int index, unsigned int value)
+{
+    assert(index<4);
+    registers[armSyscallMapping[index]]=value;
+}
+
+inline void portableSwitchToUserspace()
+{
+    asm volatile("movs r3, #1\n\t"
+                 "svc  0"
+                 :::"r3");
+}
+
+#endif //WITH_PROCESSES
 
 /**
  * \}
