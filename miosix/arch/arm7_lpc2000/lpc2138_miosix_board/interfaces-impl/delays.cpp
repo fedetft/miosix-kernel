b81b2afc925184f0d7745935e1bdc809f3b5f034
Terraneo Federico <fede.tft@miosix.org>
2024-06-21 23:33:18 +0200

    Recalibrate lpc2138 delays

--- miosix/arch/arm7_lpc2000/lpc2138_miosix_board/interfaces-impl/delays.cpp
+++ miosix/arch/arm7_lpc2000/lpc2138_miosix_board/interfaces-impl/delays.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2010 by Terraneo Federico                               *
+ *   Copyright (C) 2010-2024 by Terraneo Federico                          *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -31,16 +31,17 @@ namespace miosix {
 
 void delayMs(unsigned int mseconds)
 {
+    register const unsigned int count=14746;
+
     for(unsigned int i=0;i<mseconds;i++)
     {
         // This delay has been calibrated to take 1 millisecond
         // if running with a 58982400Hz CPU clock. It is written in assembler
         // to be independent on compiler optimization settings
-        asm volatile("           ldr   r1, =11788 \n"
-                     "           mov   r2, #0     \n"
-                     "___loop_m: cmp   r2, r1     \n"
-                     "           addlo r2, r2, #1 \n"
-                     "           blo   ___loop_m  \n":::"r1","r2");
+        asm volatile("    movs  r1, %0     \n"
+                     "    .align 2         \n" //4-byte aligned inner loop
+                     "1:  subs  r1, r1, #1 \n" //Loop takes 4 cycles
+                     "    bpl   1b         \n"::"r"(count):"r1");
     }
 }
 
@@ -49,13 +50,12 @@ void delayUs(unsigned int useconds)
     // This delay has been calibrated to take x microseconds
     // if running with a 58982400Hz CPU clock. It is written in assembler
     // to be independent on compiler optimization settings
-    asm volatile("           ldr   r1, =12    \n"
-                 "           mul   r2, %0, r1 \n"
-                 "           mov   r1, #0     \n"
-                 "___loop_u: cmp   r1, r2     \n"
-                 "           addlo r1, r1, #1 \n"
-                 "           blo   ___loop_u  \n"::"r"(useconds):"r1","r2");
-
+    asm volatile("    movs  r1, #15    \n"
+                 "    mul   r1, %0, r1 \n"
+                 "    .align 2         \n" //4-byte aligned inner loop
+                 "1:  subs  r1, r1, #1 \n" //Loop takes 4 cycles
+                 "    bpl   1b         \n"::"r"(useconds):"r1");
 }
 
 } //namespace miosix
+
