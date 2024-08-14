d6ec9f6f5de384a113cc817143fb96ba4a9aea30
Alberto Nidasio <nidasioalberto@gmail.com>
2024-08-14 16:12:07 +0200

    Created `miosix_add_process` CMake function to easily define processes

---
+++ miosix/cmake/AddProcess.cmake
@@ -0,0 +1,57 @@
+# Copyright (C) 2024 by Skyward
+#
+# This program is free software; you can redistribute it and/or
+# it under the terms of the GNU General Public License as published
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# As a special exception, if other files instantiate templates or use
+# macros or inline functions from this file, or you compile this file
+# and link it with other works to produce a work based on this file,
+# this file does not by itself cause the resulting work to be covered
+# by the GNU General Public License. However the source code for this
+# file must still be made available in accordance with the GNU
+# Public License. This exception does not invalidate any other
+# why a work based on this file might be covered by the GNU General
+# Public License.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+
+# Function to link the Miosix libraries to a target and register the build command
+function(miosix_add_process TARGET SOURCES)
+    # Define the executable with its sources
+    add_executable(${TARGET} ${SOURCES})
+
+    # Link syscalls and other libraries
+    target_link_libraries(${TARGET} PRIVATE
+        -Wl,--start-group syscalls stdc++ c m gcc atomic -Wl,--end-group
+    )
+
+    # Strin unnecessary sections from the ELF
+    add_custom_command(
+        TARGET ${TARGET} POST_BUILD
+        COMMAND arm-miosix-eabi-strip $<TARGET_FILE:${TARGET}>
+        COMMENT "Stripped $<TARGET_FILE_NAME:${TARGET}>"
+    )
+
+    # Run mx-postlinker to strip the section header, string table and
+    # setting Miosix specic options in the dynamic segnment
+    add_custom_command(
+        TARGET ${TARGET} POST_BUILD
+        COMMAND mx-postlinker $<TARGET_FILE:${TARGET}> --ramsize=16384 --stacksize=2048 --strip-sectheader
+        COMMENT "Runned mx-postlinker on $<TARGET_FILE_NAME:${TARGET}>"
+    )
+
+    # Move the ELF into the bin directory
+    add_custom_command(
+        TARGET ${TARGET} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${TARGET}> ${CMAKE_BINARY_DIR}/bin/$<TARGET_FILE_BASE_NAME:${TARGET}>
+        COMMENT "Moved $<TARGET_FILE_NAME:${TARGET}> into bin directory"
+    )
+endfunction()
