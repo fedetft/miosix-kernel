f5f7745216ebd88d001293b81786a39715ef990d
Alberto Nidasio <nidasioalberto@gmail.com>
2024-10-22 16:41:32 +0200

    Rename variables in gcc.cmake

--- miosix/cmake/Toolchains/gcc.cmake
+++ miosix/cmake/Toolchains/gcc.cmake
@@ -1,7 +1,7 @@
 # Copyright (C) 2024 by Skyward
 #
-# This program is free software; you can redistribute it and/or 
-# it under the terms of the GNU General Public License as published 
+# This program is free software; you can redistribute it and/or
+# it under the terms of the GNU General Public License as published
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
@@ -15,8 +15,8 @@
 # and link it with other works to produce a work based on this file,
 # this file does not by itself cause the resulting work to be covered
 # by the GNU General Public License. However the source code for this
-# file must still be made available in accordance with the GNU 
-# Public License. This exception does not invalidate any other 
+# file must still be made available in accordance with the GNU
+# Public License. This exception does not invalidate any other
 # why a work based on this file might be covered by the GNU General
 # Public License.
 #
@@ -24,34 +24,35 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 
 # Add the miosix/cmake path to find the Miosix.cmake platform file
-# that defines the Miosix system name 
+# that defines the Miosix system name
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/..)
 
 # Tell CMake that we are building for an embedded ARM system
 set(CMAKE_SYSTEM_NAME Miosix)
 
 # Select compiler
-set(PREFIX arm-miosix-eabi-)
+set(MIOSIX_PREFIX      arm-miosix-eabi)
 
 # From compiler prefix form the name of the compiler and other tools
-set(CMAKE_ASM_COMPILER ${PREFIX}as)
-set(CMAKE_C_COMPILER   ${PREFIX}gcc)
-set(CMAKE_CXX_COMPILER ${PREFIX}g++)
-set(CMAKE_AR           ${PREFIX}ar)
-set(CMAKE_OBJCOPY      ${PREFIX}objcopy)
-set(CMAKE_OBJDUMP      ${PREFIX}objdump)
-set(CMAKE_SIZE         ${PREFIX}size)
+set(CMAKE_ASM_COMPILER ${MIOSIX_PREFIX}-as)
+set(CMAKE_C_COMPILER   ${MIOSIX_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${MIOSIX_PREFIX}-g++)
+set(CMAKE_AR           ${MIOSIX_PREFIX}-ar)
+set(CMAKE_OBJCOPY      ${MIOSIX_PREFIX}-objcopy)
+set(CMAKE_OBJDUMP      ${MIOSIX_PREFIX}-objdump)
+set(CMAKE_SIZE         ${MIOSIX_PREFIX}-size)
+set(MIOSIX_READELF     ${MIOSIX_PREFIX}-readelf)
 
 # Optimization flags for each language and build configuration
 set(CMAKE_ASM_FLAGS_DEBUG "")
-set(CMAKE_C_FLAGS_DEBUG "-Og -g")
-set(CMAKE_CXX_FLAGS_DEBUG "-Og -g")
+set(CMAKE_C_FLAGS_DEBUG "-g -O0")
+set(CMAKE_CXX_FLAGS_DEBUG "-g -O0")
 set(CMAKE_ASM_FLAGS_RELEASE "")
 set(CMAKE_C_FLAGS_RELEASE "-O2")
 set(CMAKE_CXX_FLAGS_RELEASE "-O2")
 set(CMAKE_ASM_FLAGS_RELWITHDEBINFO "")
-set(CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
+set(CMAKE_C_FLAGS_RELWITHDEBINFO "-g -O2")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g -O2")
 set(CMAKE_ASM_FLAGS_MINSIZEREL "")
 set(CMAKE_C_FLAGS_MINSIZEREL "-Os")
 set(CMAKE_CXX_FLAGS_MINSIZEREL "-Os")
