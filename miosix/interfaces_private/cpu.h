822c888e83da31fadfde9ade3ef91ec0f82ad1a3
Terraneo Federico <fede.tft@miosix.org>
2024-10-23 15:22:52 +0200

    Removed IRQportableFinishKernelStartup() from cpu.h
    This function declaration was a leftover from commit e813775f7 from 2021-08-29, not defined nor used anywhere.
    Also updated comments to IRQportableStartKernel() to reflect the implemented behavior.

--- miosix/interfaces_private/cpu.h
+++ miosix/interfaces_private/cpu.h
@@ -60,29 +60,18 @@ void IRQsystemReboot();
  * \internal
  * Called by miosix::start_kernel to handle the architecture-specific part of
  * initialization. It is used by the kernel, and should not be used by end users.
- * It is ensured that the miosix::kernel_started flag false during the execution
- * of this function. Upon return, miosix::kernel_started is set to be true and
- * IRQportableFinishKernelStartup is called immediately.
- * A motivation for this flow could be that it allows running of general purpose
- * driver classes that would be ran either before or after start of the kernel.
- * Probably these drivers may need to disable interrupts using InterruptDisableLock
- * in the case that they are initialized after kernel's startup, while using
- * InterruptDisableLock is error-prone when the kernel_started flag is true and
- * the kernel is not fully started yet.
+ *
+ * This function should at minimum enable interrupts and perform the first
+ * context switch.
+ *
+ * NOTE: the miosix::kernel_started flag is set to true before calling this
+ * function, even though the kernel cannot be considered started until this
+ * function completes, causing the first context switch.
+ * For this reason this function must be written with great care not to call
+ * code that uses InterruptDisableLock, such as general purpose driver classes
+ * that would be ran either before or after start of the kernel.
  */
 void IRQportableStartKernel();
-
-/**
- * \internal
- * This function is called right after IRQportableStartKernel by
- * miosix::start_kernel. The miosix::kernel_started is set to true at this
- * stage.
- * A typical behaviour that's expected is to :
- * 1) Enable falut IRQ
- * 2) Enable IRQs
- * 3) miosix::Thread::yield();
- */
-void IRQportableFinishKernelStartup();
 
 /**
  * \internal
