82ed3db43ff9db3bbb16d895875cae5223a3243c
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2025-05-20 01:53:02 +0200

    Documentation for the RP2040 SPI driver.
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

--- miosix/arch/common/drivers/rp2040_spi.h
+++ miosix/arch/common/drivers/rp2040_spi.h
@@ -33,25 +33,101 @@
 
 namespace miosix {
 
+/**
+ * RP2040 DMA-enabled driver for the PL022 SPI hardware. Master mode only.
+ */
 class RP2040PL022SPI
 {
 public:
+    /**
+     * Constructor.
+     * \param number The ID of the peripheral (0 or 1)
+     * \param bitrate Initially configured serial clock frequency upper bound in
+     * Hz. The actual clock rate programmed might be lower.
+     * \param spo Clock polarity. `false' keeps the SCK pin low when the line is
+     * idle, while `true' keeps SCK high instead.
+     * \param sph Clock phase. `false' if data in/out must be sampled at the
+     * rising edge of the clock, `true' if it must be sampled at the falling
+     * edge.
+     * \param si Serial in pin.
+     * \param so Serial out pin.
+     * \param sck Serial clock pin.
+     * \param ce Chip enable pin (optional).
+     */
     RP2040PL022SPI(int number, unsigned int bitrate, bool spo, bool sph,
                    GpioPin si, GpioPin so, GpioPin sck, GpioPin ce);
+    /**
+     * Destructor.
+     */
     ~RP2040PL022SPI();
 
+    /**
+     * Changes the current serial clock to the specified one.
+     * \param The clock rate to be programmed in Hz. This is an upper bound;
+     * the actual clock rate might be lower.
+     */
     void setBitrate(unsigned int bitrate);
 
+    /**
+     * Send and receive a single word.
+     * \param data The word to be transmitted
+     * \param wordSize The word size; only the least significant wordSize bits
+     * of data will be transmitted, and only wordSize bits are going to be
+     * received.
+     * This parameter specifies the number of clocks that correspond to each
+     * word and must be correct according to what the receiving peripheral
+     * expects.
+     * By default it is 8 bits.
+     * \returns The data coming back from the SI pin.
+     */
     unsigned short sendRecv(unsigned short data, unsigned wordSize=8);
 
+    /**
+     * Send and receive a string of words.
+     * \param send The string of words to be transmitted.
+     * \param recv Buffer for the words received.
+     * \param len The number of words to be sent and simultaneously received.
+     * \param wordSize The word size; only the least significant wordSize bits
+     * of each data word will be transmitted, and only wordSize bits are going
+     * to be received for each data word.
+     * This parameter specifies the number of clocks that correspond to each
+     * word and must be correct according to what the receiving peripheral
+     * expects.
+     * By default it is 8 bits.
+     */
     void sendRecv(const unsigned short send[], unsigned short recv[], size_t len, unsigned wordSize=8);
     void sendRecv(const unsigned char send[], unsigned char recv[], size_t len, unsigned wordSize=8);
 
+    /**
+     * Send a string of words, ignoring the response being trasmitted back.
+     * \param send The string of words to be transmitted.
+     * \param len The number of words to be sent.
+     * \param wordSize The word size; only the least significant wordSize bits
+     * of each data word will be transmitted.
+     * This parameter specifies the number of clocks that correspond to each
+     * word and must be correct according to what the receiving peripheral
+     * expects.
+     * By default it is 8 bits.
+     */
     void send(const unsigned short send[], size_t len, unsigned wordSize=8);
     void send(const unsigned char send[], size_t len, unsigned wordSize=8);
 
+    /**
+     * Receive a string of words, while repeatedly sending the same data.
+     * \param recv Buffer for the words received.
+     * \param len The number of words to be received.
+     * \param wordSize The word size; only the least significant wordSize bits
+     * of each data word will be transmitted.
+     * This parameter specifies the number of clocks that correspond to each
+     * word and must be correct according to what the receiving peripheral
+     * expects.
+     * By default it is 8 bits.
+     * \param sendDummy Word to be sent to keep the transmission going and
+     * the clock clocking.
+     */
     void recv(unsigned short recv[], size_t len, unsigned wordSize=8, unsigned short sendDummy=0xFFFF);
     void recv(unsigned char recv[], size_t len, unsigned wordSize=8, unsigned short sendDummy=0xFF);
+    
 private:
     void IRQhandleInterrupt();
     void IRQhandleDmaInterrupt();
