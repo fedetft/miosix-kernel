6d420f436335e0f310bb8074b58635687068d5a8
Niccolò Betto <niccolo.betto@gmail.com>
2025-06-08 10:15:24 +0200

    GCC 14.2.0 mp4.0: Newlib and libgomp additional patches
    newlib
    * Add forward declaration of pthread_mutex functions in sys/lock.h
    * Add missing forward declarations needed because of GCC 14 compiling C code as C23 by default
    
    libgomp
    * Force omp_alloctrait_key_t size to 4 bytes
    
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

--- miosix/_tools/compiler/gcc-14.2.0-mp4.0/install-script.sh
+++ miosix/_tools/compiler/gcc-14.2.0-mp4.0/install-script.sh
@@ -200,8 +200,8 @@ extract()
 }
 
 extract 'binutils' $BINUTILS.tar.xz patches/binutils.patch
-extract 'gcc' $GCC.tar.xz patches/gcc.patch patches/libgomp-fix-crash.patch
-extract 'newlib' $NEWLIB.tar.gz patches/newlib.patch
+extract 'gcc' $GCC.tar.xz patches/gcc.patch patches/libgomp-fix-crash.patch patches/libgomp_gcc14.patch
+extract 'newlib' $NEWLIB.tar.gz patches/newlib.patch patches/newlib_gcc14.patch
 extract 'gdb' $GDB.tar.xz
 extract 'gmp' $GMP.tar.xz
 extract 'mpfr' $MPFR.tar.xz
