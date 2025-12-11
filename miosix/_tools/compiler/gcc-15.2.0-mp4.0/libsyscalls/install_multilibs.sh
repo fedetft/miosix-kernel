3923700d565df1b9472ad787b578dccccdfac2d5
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2025-12-11 17:02:00 +0100

    libsyscalls: Pass variables to makefiles directly instead of relying on the environment because sudo may clean it up. Propagate errors
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

--- miosix/_tools/compiler/gcc-15.2.0-mp4.0/libsyscalls/install_multilibs.sh
+++ miosix/_tools/compiler/gcc-15.2.0-mp4.0/libsyscalls/install_multilibs.sh
@@ -12,17 +12,12 @@ quit() {
 if [[ -z "$PREFIX" ]]; then
     quit PREFIX must be a toolchain prefix path like '/opt/arm-miosix-eabi'
 fi
-# Ensure PREFIX and DESTDIR are visible to the makefile
-export PREFIX=$PREFIX
-export DESTDIR=$DESTDIR
-# For now support arm-miosix-eabi only, in the future this will change
-export TARGET=arm-miosix-eabi
 
 all_multilibs=$(arm-miosix-eabi-gcc --print-multi-lib)
 for libspec in $all_multilibs; do
     libspec_parts=(${libspec//;/ })
-    export MULTILIB_PATH=${libspec_parts[0]}
-    export MULTILIB_FLAGS=${libspec_parts[1]//@/ -}
+    MULTILIB_PATH=${libspec_parts[0]}
+    MULTILIB_FLAGS=${libspec_parts[1]//@/ -}
     echo "Multilib path $MULTILIB_PATH (flags:$MULTILIB_FLAGS)"
     if [[ "$MULTILIB_PATH" == '.' ]]; then
         echo 'Root multilib, skipped.'
@@ -32,7 +27,12 @@ for libspec in $all_multilibs; do
         make clean
         # TODO: quit on error, current we can't do that because armv8-m.base
         # libs don't build and we want to ignore that.
-        $SUDO make install
+        $SUDO make install \
+            PREFIX="$PREFIX" \
+            DESTDIR="$DESTDIR" \
+            TARGET=arm-miosix-eabi \
+            MULTILIB_PATH="$MULTILIB_PATH" \
+            MULTILIB_FLAGS="$MULTILIB_FLAGS" || quit 'Compilation error'
     fi
 done
 make clean
