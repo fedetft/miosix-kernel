226f7aaa6c69a2a922bde4dc54682550f9782213
Niccolò Betto <niccolo.betto@gmail.com>
2026-01-19 00:10:09 +0100

    Initial STM32 ETH peripheral driver and DMA descriptor structs
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

---
+++ miosix/arch/drivers/stm32_eth.cpp
@@ -0,0 +1,201 @@
+/***************************************************************************
+ *   Copyright (C) 2025-2026 by Niccolò Betto                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   As a special exception, if other files instantiate templates or use   *
+ *   macros or inline functions from this file, or you compile this file   *
+ *   and link it with other works to produce a work based on this file,    *
+ *   this file does not by itself cause the resulting work to be covered   *
+ *   by the GNU General Public License. However the source code for this   *
+ *   file must still be made available in accordance with the GNU General  *
+ *   Public License. This exception does not invalidate any other reasons  *
+ *   why a work based on this file might be covered by the GNU General     *
+ *   Public License.                                                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#include "stm32_eth.h"
+
+#include <interfaces/arch_registers.h>
+#include <interfaces/endianness.h>
+#include <interfaces/interrupts.h>
+#include <kernel/lock.h>
+#include <kernel/logging.h>
+
+namespace miosix::stm32_eth {
+
+void RxDmaDescriptor::syncToCpu() {
+#if defined(STM32F7) || defined(STM32H7)
+    SCB_InvalidateDCache_by_Addr(reinterpret_cast<uint32_t *>(buffer),
+                                 size & 0x1FFF);
+    SCB_InvalidateDCache_by_Addr(reinterpret_cast<uint32_t *>(this),
+                                 sizeof(RxDmaDescriptor));
+#endif
+}
+
+void RxDmaDescriptor::syncToDma() {
+#if defined(STM32F7) || defined(STM32H7)
+    SCB_CleanDCache_by_Addr(reinterpret_cast<uint32_t *>(this),
+                            sizeof(RxDmaDescriptor));
+#endif
+}
+
+void TxDmaDescriptor::syncToCpu() {
+#if defined(STM32F7) || defined(STM32H7)
+    SCB_InvalidateDCache_by_Addr(reinterpret_cast<uint32_t *>(this),
+                                 sizeof(RxDmaDescriptor));
+#endif
+}
+
+void TxDmaDescriptor::syncToDma() {
+#if defined(STM32F7) || defined(STM32H7)
+    SCB_CleanDCache_by_Addr(reinterpret_cast<uint32_t *>(buffer), size);
+    SCB_CleanDCache_by_Addr(reinterpret_cast<uint32_t *>(this),
+                            sizeof(RxDmaDescriptor));
+#endif
+}
+
+bool STM32Ethernet::IrqStatus::rx() { return *reg & ETH_DMASR_RS; }
+
+void STM32Ethernet::IrqStatus::clearRx() {
+    *reg &= (ETH_DMASR_RS | ETH_DMASR_NIS);
+}
+
+bool STM32Ethernet::IrqStatus::tx() { return *reg & ETH_DMASR_TS; }
+
+void STM32Ethernet::IrqStatus::clearTx() {
+    *reg &= (ETH_DMASR_TS | ETH_DMASR_NIS | ETH_DMASR_TBUS | ETH_DMASR_ETS);
+}
+
+void STM32Ethernet::init(RxDmaDescriptor *rxDesc, TxDmaDescriptor *txDesc,
+                         EthernetIrqHandler irqHandler, void *irqArg) {
+    SYSCFG->PMC = 0; // Select MII interface, must be done before enabling ETH
+
+    {
+        miosix::FastGlobalIrqLock dLock;
+        RCC->AHB1ENR |= RCC_AHB1ENR_ETHMACEN | RCC_AHB1ENR_ETHMACRXEN |
+                        RCC_AHB1ENR_ETHMACTXEN |
+                        RCC_AHB1ENR_ETHMACPTPEN; // TODO: do we need it?
+        RCC_SYNC();
+    }
+
+    ETH->MACCR = 0   // Do not strip FCS
+                 | 0 // Enable watchdog to cut RX packets to 2048 bytes
+                 | 0 // Enable watchdog to cut TX packets to 2048 bytes
+                 | 0 // Standard IFG 96 bit
+                 | 0 // Enable carrier sense checking
+                 | ETH_MACCR_FES // 100MBit/s mode
+                 | ETH_MACCR_DM  // Full duplex
+                 | ETH_MACCR_IPCO // Check checksum of IPv4 frames
+                 | 0;
+
+    // No source filtering, no promiscuous mode, don't forward flow control
+    // frames
+    ETH->MACFFR = 0;
+
+    // TODO: how long is a slot time? PT and PLT taken from example in
+    // datasheet
+    ETH->MACFCR = (256 << 16)       // PT=256 slot times
+                  | (1 << 4)        // PLT=1 (28 slot times)
+                  | ETH_MACFCR_UPFD // Detect also uncast pause frames
+                  | ETH_MACFCR_RFCE // Enable honoring received pause frames
+                  | 0;              // Disable sending pause frames
+
+    ETH->MACIMR = ETH_MACIMR_TSTIM    // Disable timestamp interrupts
+                  | ETH_MACIMR_PMTIM; // Disable power management interrupts
+
+    // TODO: set proper MAC based on UID
+    auto setMac = [](unsigned long long mac) {
+        ETH->MACA0HR = 1U << 31 | swapBytes16(mac & 0xffff);
+        ETH->MACA0LR = swapBytes32(mac >> 16);
+    };
+
+    setMac(0x0080e1000000ull); // Set a MAC address with ST OUI
+
+    // Disable counter interrupts
+    ETH->MMCRIMR = ETH_MMCRIMR_RGUFM | ETH_MMCRIMR_RFAEM | ETH_MMCRIMR_RFCEM;
+    ETH->MMCTIMR = ETH_MMCTIMR_TGFM | ETH_MMCTIMR_TGFMSCM | ETH_MMCTIMR_TGFSCM;
+
+    ETH->DMABMR = ETH_DMABMR_PBL_8Beat // PBL=8 (default)
+                  | ETH_DMABMR_EDE;    // Enhanced descriptors enabled
+
+    ETH->DMAOMR = ETH_DMAOMR_RSF | ETH_DMAOMR_TSF;
+
+    // Set up DMA descriptor lists
+    ETH->DMARDLAR = reinterpret_cast<uint32_t>(rxDesc);
+    ETH->DMATDLAR = reinterpret_cast<uint32_t>(txDesc);
+
+    // Setup DMA interrupt
+    ETH->DMAIER = ETH_DMAIER_NISE   // Normal interrupt summary
+                  | ETH_DMAIER_RIE  // RX interrupt
+                  | ETH_DMAIER_TIE; // TX interrupt
+
+    if (irqHandler) {
+        miosix::GlobalIrqLock gLock;
+        IRQregisterIrq(gLock, ETH_IRQn, irqHandler, irqArg);
+    }
+
+    // Finally enable MAC and DMA
+    ETH->MACCR |= ETH_MACCR_TE | ETH_MACCR_RE;    // Enable MAC TX and RX
+    ETH->DMAOMR |= ETH_DMAOMR_ST | ETH_DMAOMR_SR; // Start DMA TX and RX
+}
+
+STM32Ethernet::IrqStatus STM32Ethernet::getIrqStatus() { return &ETH->DMASR; }
+
+void STM32Ethernet::pollRx() {
+    ETH->DMARPDR = 0; // Poll RX
+}
+
+void STM32Ethernet::restartRx() {
+    ETH->DMAOMR |= ETH_DMAOMR_SR; // Start RX
+}
+
+void STM32Ethernet::pollTx() {
+    ETH->DMATPDR = 0; // Poll TX
+}
+
+void STM32Ethernet::restartTx() {
+    ETH->DMAOMR |= ETH_DMAOMR_ST; // Start TX
+}
+
+void STM32Ethernet::IRQprintStatus() {
+    IRQerrorLog("ETH->DMASR: ");
+
+#define LOG_IRQ_BIT(bit)                                                       \
+    if (ETH->DMASR & ETH_DMASR_##bit) {                                        \
+        IRQerrorLog(#bit " ");                                                 \
+    }
+
+    LOG_IRQ_BIT(NIS);
+    LOG_IRQ_BIT(AIS);
+    LOG_IRQ_BIT(ERS);
+    LOG_IRQ_BIT(FBES);
+    LOG_IRQ_BIT(ETS);
+    LOG_IRQ_BIT(RWTS);
+    LOG_IRQ_BIT(RPSS);
+    LOG_IRQ_BIT(RBUS);
+    LOG_IRQ_BIT(RS);
+    LOG_IRQ_BIT(TUS);
+    LOG_IRQ_BIT(ROS);
+    LOG_IRQ_BIT(TJTS);
+    LOG_IRQ_BIT(TBUS);
+    LOG_IRQ_BIT(TPSS);
+    LOG_IRQ_BIT(TS);
+
+    IRQerrorLog("\r\n");
+
+#undef LOG_IRQ_BIT
+}
+
+} // namespace miosix::stm32_eth
---
+++ miosix/arch/drivers/stm32_eth.h
@@ -0,0 +1,241 @@
+/***************************************************************************
+ *   Copyright (C) 2025-2026 by Niccolò Betto                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   As a special exception, if other files instantiate templates or use   *
+ *   macros or inline functions from this file, or you compile this file   *
+ *   and link it with other works to produce a work based on this file,    *
+ *   this file does not by itself cause the resulting work to be covered   *
+ *   by the GNU General Public License. However the source code for this   *
+ *   file must still be made available in accordance with the GNU General  *
+ *   Public License. This exception does not invalidate any other reasons  *
+ *   why a work based on this file might be covered by the GNU General     *
+ *   Public License.                                                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#pragma once
+
+#include <cstdint>
+
+namespace miosix::stm32_eth {
+
+struct alignas(uint32_t) RxDmaDescriptor {
+    // Returns true if the descriptor is owned by the DMA
+    bool ownedByDma() const { return status & (1U << 31); }
+
+    // Marks the descriptor as owned by DMA and clears status
+    void setDmaOwned() { status = (1U << 31); }
+
+    bool first() const { return status & (1U << 9); }
+
+    bool last() const { return status & (1U << 8); }
+
+    // Error summary bit is only valid if last is set
+    bool error() const { return status & (1U << 15); }
+
+    // Frame length in bytes, including CRC, valid only if last is set
+    uint16_t frameLength() const { return (status >> 16) & 0x3FFF; }
+
+    // Sets the buffer pointed to by this descriptor
+    void setBuffer(void *buf, uint16_t bufSize) {
+        buffer = buf;
+        // Only set buffer1 size bits [12:0], keeps end-of-ring bit if set
+        size = (size & ~0x1FFF) | (bufSize & 0x1FFF);
+    }
+
+    // Marks this descriptor as the last one in the descriptor ring
+    void setEndOfRing() { size |= (1U << 15); }
+
+    /**
+     * DMA -> CPU memory sync.
+     * Synchronizes the descriptor and buffer memory so that the CPU reads
+     * the latest data written by DMA.
+     * \note Assumes the descriptor is 32-byte aligned
+     * \note Assumes the buffer is 32-byte aligned
+     */
+    void syncToCpu();
+
+    /**
+     * CPU -> DMA memory sync.
+     * Synchronizes the descriptor and buffer memory so that DMA reads the
+     * latest data written by CPU.
+     * \note Assumes the descriptor is 32-byte aligned
+     */
+    void syncToDma();
+
+  private:
+    volatile uint32_t status = 0;
+    uint32_t size = 0;
+    void *buffer = nullptr;
+    void *buffer2 = nullptr; // unused
+    volatile uint32_t statusExt = 0;
+    uint32_t reserved = 0;
+    volatile uint32_t timestamp[2] = {};
+};
+static_assert(sizeof(RxDmaDescriptor) == 32);
+
+struct alignas(uint32_t) TxDmaDescriptor {
+    // Returns true if the descriptor is owned by the DMA
+    bool ownedByDma() const { return control & (1U << 31); }
+
+    // Marks the descriptor as owned by DMA
+    void setDmaOwned() { control |= (1U << 31); }
+
+    // Marks the descriptor as owned by CPU
+    void setCpuOwned() { control &= ~(1U << 31); }
+
+    void assignBuffer(void *buf, uint16_t bufSize, bool first, bool last,
+                      bool endOfRing) {
+        constexpr uint32_t Checksum = 0b00;
+
+        control = 0U |                // Clear status bits
+                  (0U << 31) |        // Owned by CPU
+                  (1U << 30) |        // Enable IRQ on full frame tx complete
+                  (last << 29) |      // Last segment
+                  (first << 28) |     // First segment
+                  (0U << 27) |        // Enable CRC insertion
+                  (0U << 26) |        // Enable pad insertion
+                  (0U << 25) |        // TX timestamp disabled
+                  (Checksum << 22) |  // Checksum insertion
+                  (endOfRing << 21) | // Ring mode
+                  (0U << 20);         // Disable chain mode
+
+        setBuffer(buf, bufSize);
+    }
+
+    bool first() const { return control & (1U << 28); }
+
+    bool last() const { return control & (1U << 29); }
+
+    uint16_t bufferSize() const { return size & 0x1FFF; }
+
+    // Sets the buffer pointed to by this descriptor
+    void setBuffer(void *buf, uint16_t bufSize) {
+        buffer = buf;
+        size = bufSize & 0x1FFF; // buffer1 size bits [12:0]
+    }
+
+    // Marks this descriptor as the last one in the descriptor ring
+    void setEndOfRing() { control |= (1U << 21); }
+
+    /**
+     * DMA -> CPU memory sync.
+     * Synchronizes the descriptor memory so that the CPU reads the
+     * latest data written by DMA.
+     * \note Assumes the descriptor is 32-byte aligned
+     */
+    void syncToCpu();
+
+    /**
+     * CPU -> DMA memory sync.
+     * Synchronizes the descriptor memory so that DMA reads the
+     * latest data written by CPU.
+     * \note Assumes the descriptor is 32-byte aligned
+     */
+    void syncToDma();
+
+  private:
+    volatile uint32_t control = 0;
+    uint32_t size = 0;
+    void *buffer = nullptr;
+    void *buffer2 = nullptr; // unused
+    uint32_t reserved[2] = {};
+    volatile uint32_t timestamp[2] = {};
+};
+static_assert(sizeof(TxDmaDescriptor) == 32);
+
+/**
+ * STM32 Ethernet hardware interface.
+ * Provides low level access to the STM32 Ethernet MAC and DMA.
+ */
+namespace STM32Ethernet {
+
+using EthernetIrqHandler = void (*)(void *);
+
+/**
+ * Ethernet IRQ status generic interface.
+ * Presents a generic interface to read Ethernet IRQ status flags, decoupled
+ * from the underlying hardware registers.
+ */
+class IrqStatus {
+  public:
+    IrqStatus(volatile uint32_t *r) : reg(r) {}
+
+    /**
+     * Returns true if an RX interrupt is pending.
+     */
+    bool rx();
+    /**
+     * Clears RX IRQ flags.
+     */
+    void clearRx();
+
+    /**
+     * Returns true if a TX interrupt is pending.
+     */
+    bool tx();
+    /**
+     * Clears TX IRQ flags.
+     */
+    void clearTx();
+
+  private:
+    volatile uint32_t *const reg; // Const pointer to maximize optimization
+};
+
+/**
+ * Initialize the Ethernet hardware (MAC and DMA).
+ * \param rxDesc pointer to the RX DMA descriptor list
+ * \param txDesc pointer to the TX DMA descriptor list
+ * \param irqHandler optional IRQ handler to register for Ethernet
+ * interrupts
+ * \param irqParam optional parameter to pass to the IRQ handler
+ */
+void init(RxDmaDescriptor *rxDesc, TxDmaDescriptor *txDesc,
+          EthernetIrqHandler irqHandler = nullptr, void *irqArg = nullptr);
+
+IrqStatus getIrqStatus();
+
+/**
+ * Polls the DMA to resume RX processing.
+ *
+ * If the DMA RX engine was suspended (e.g. no RX descriptors available),
+ * this function notifies the DMA to fetch the next descriptor and resume
+ * reception.
+ */
+void pollRx();
+
+void restartRx();
+
+/**
+ * Polls the DMA to resume TX processing.
+ *
+ * If the DMA TX engine was suspended (e.g. all TX descriptors were
+ * processed, no descriptors to send), this function notifies the DMA to
+ * fetch the next descriptor and resume transmission.
+ */
+void pollTx();
+
+void restartTx();
+
+/**
+ * Prints the status register ETH->DMASR to the default console for debugging
+ * purposes. Can only be called from an IRQ context.
+ */
+[[maybe_unused]] void IRQprintStatus();
+
+}; // namespace STM32Ethernet
+
+} // namespace miosix::stm32_eth
