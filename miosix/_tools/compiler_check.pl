673d1aa5e560377a46018c7f10d92dc3fea63a6e
Terraneo Federico <fede.tft@miosix.org>
2026-03-04 23:23:19 +0100

    Print half-decent error messages when compiling miosix 3 with gcc 9

---
+++ miosix/_tools/compiler_check.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+# usage: compiler_check.pl <compiler name>
+# example: perl compiler_check.pl arm-miosix-eabi-gcc
+# returns: 0 if the compiler version is compatible with the current kernel
+#
+# This program is written in perl to help make, which can't even do greater-than
+# comparisons without using the shell, let alone regex. Miosix Makefiles need to
+# work also on Windows, so we can't use bash and use perl instead.
+
+use warnings;
+use strict;
+
+my $fullversion=`$ARGV[0] -E -dM - </dev/null`;
+my $patch_major=-1;
+for(split /\n/, $fullversion)
+{
+    next unless(/#define _MIOSIX_GCC_PATCH_MAJOR (\d+)/);
+    $patch_major=$1;
+}
+if($patch_major>=4)
+{
+    print "0";
+} else {
+    print "1";
+}
