4f29b396dc3cbeff9d8a46814fefb57ba9fe552d
Terraneo Federico <fede.tft@miosix.org>
2026-03-13 11:48:07 +0100

    Improve Linux compiler installer

--- miosix/_tools/compiler/gcc-15.2.0-mp4.1/installers/linux/installer.sh
+++ miosix/_tools/compiler/gcc-15.2.0-mp4.1/installers/linux/installer.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-echo "Checking if a previous version of the Miosix Toolchain is installed"
-echo "and uninstalling it ..."
-
-./uninstall.sh
-
 quit() {
 	echo $1
 	exit 1
 }
 
+which sudo > /dev/null || quit "Error: sudo is missing, please install it and try again"
+
+echo "Checking if a previous version of the Miosix Toolchain is installed"
+echo "and uninstalling it ..."
+
+./uninstall.sh
+
 echo "Installing the Miosix toolchain ..."
+# NOTE: some distros may not have /opt anymore
+[[ -d /opt ]] || sudo mkdir /opt
 # NOTE: "" around pwd as the directory may contain spaces
 sudo cp -R "`pwd`" /opt/arm-miosix-eabi			|| quit "Error: can't install to /opt/arm-miosix-eabi"
 sudo ln -s /opt/arm-miosix-eabi/bin/* /usr/bin	|| quit "Error: can't make symlinks to /usr/bin"
