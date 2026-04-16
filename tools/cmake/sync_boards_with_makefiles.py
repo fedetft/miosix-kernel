b28cf945b6e285c4beecf56e3ed3118c4e11a69d
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2026-04-16 23:16:39 +0200

    CMake tools: add copyright notices and comments for explaining the purpose of the tools.
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

--- tools/cmake/sync_boards_with_makefiles.py
+++ tools/cmake/sync_boards_with_makefiles.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
+# Copyright (C) 2026 by Daniele Cattaneo
+#
+# This program is free software; you can redistribute it and/or
+# it under the terms of the GNU General Public License as published
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# As a special exception, if other files instantiate templates or use
+# macros or inline functions from this file, or you compile this file
+# and link it with other works to produce a work based on this file,
+# this file does not by itself cause the resulting work to be covered
+# by the GNU General Public License. However the source code for this
+# file must still be made available in accordance with the GNU
+# Public License. This exception does not invalidate any other
+# why a work based on this file might be covered by the GNU General
+# Public License.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+
+# This tool regenerates all miosix/arch/board/.../CMakeLists.txt files
+# by converting the Makefile.inc files to equivalent CMake syntax and variable
+# names.
 
 from pathlib import Path
 from miosix_makefile_parser import *
