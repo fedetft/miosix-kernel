8085978379edf94d4d2f8ebd2d4a3484dec39bfa
Matteo Rogora <matteo.rogora@live.it>
2026-08-25 18:55:35 +0200

    Added debugger template
    Signed-off-by: Terraneo Federico <fede.tft@miosix.org>

---
+++ templates/debugger/main.cpp
@@ -0,0 +1,57 @@
+#include <miosix.h>
+#include <sys/wait.h>
+#include <spawn.h>
+
+// // Enable processes uncommenting
+// #define WITH_PROCESSES
+// // in miosix_settings.h
+
+// // Enable process debugger uncommenting
+// #define PROCESS_DEBUGGER
+// // in miosix_settings.h
+
+// // Enable auxiliar tty defining
+// #define AUX_SERIAL "auxtty"
+// // in miosix_settings.h
+
+#include <debugger/debugger.h>
+
+int main() {
+
+    // Spawn a process
+    pid_t pid,ec;
+    const char *arg[] = { "/bin/helloworld", nullptr };
+    const char *env[] = { nullptr };
+
+    ec = posix_spawn(&pid,arg[0],NULL,NULL,(char* const*)arg,(char* const*)env);
+    if(ec!=0)
+    {
+        perror("posix_spawn");
+        return 1;
+    }
+
+    pid=wait(&ec);
+    iprintf("pid %d exited\n",pid);
+    if(WIFEXITED(ec))
+    {
+        iprintf("Exit code is %d\n",WEXITSTATUS(ec));
+    } else if(WIFSIGNALED(ec)) {
+        if(WTERMSIG(ec)==SIGSEGV) iprintf("Process segfaulted\n");
+        else iprintf("Process terminated due to an error\n");
+    }
+
+    // Start debugger
+
+    // Reference executable for debugger is in './process_template/bin'
+
+    // // Alternatively, pass the path directly
+    // debugger.listen("/dev/auxtty");
+    miosix::Debugger debugger;
+    int serial = open("/dev/auxtty");
+    debugger.listen(serial);
+    close(serial);
+
+    // Code here runs if debugger fails
+    iprintf("Debugger failed\n");
+
+}
---
+++ templates/debugger/process_template/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Hello world!" << std::endl;
+    }
+}
